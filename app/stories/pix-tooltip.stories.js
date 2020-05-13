ad7d1b09f20cb8bc284b867419edbaba51d972c4
Krysthalia <ang.schall@gmail.com>
2020-05-13 18:29:00 +0200

    Add tooltip story

---
+++ app/stories/pix-tooltip.stories.js
@@ -0,0 +1,15 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+export default { title: 'Tooltip' };
+
+export const tooltip = () => {
+  return {
+    template: hbs`<br><br>
+      <PixTooltip
+         @text='In accumsan scelerisque sapien, et lacinia nisi efficitur a.'
+         @position='right'>
+         <button>Hover me!</button>
+      </PixTooltip>
+    `,
+  }
+};
