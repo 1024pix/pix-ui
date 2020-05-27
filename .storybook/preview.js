d08f4b4a47917d3b15bd7305924fe7498ca743a7
Krysthalia <ang.schall@gmail.com>
2020-05-27 16:42:15 +0200

    example of decorator use

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,42 @@
 import { addParameters } from '@storybook/ember';
 import storybookCustomTheme from './storybook-custom-theme';
+import { addDecorator } from '@storybook/ember';
 
 addParameters({
   options: {
     theme: storybookCustomTheme,
   },
+});
+
+
+addDecorator(storyFn => {
+  const { template, context } = storyFn();
+
+  // This adds styling to the Canvas tab.
+  const styles = {
+    style: {
+      margin: '20px',
+      'background-color': 'skyblue',
+    },
+  };
+
+  // Create a div to wrap the Canvas tab with the applied styles.
+  const element = document.createElement('div');
+  Object.assign(element.style, styles.style);
+
+  const innerElement = document.createElement('div');
+  const wormhole = document.createElement('div');
+  wormhole.setAttribute('id', 'ember-basic-dropdown-wormhole');
+  innerElement.appendChild(wormhole);
+
+  element.appendChild(innerElement);
+  innerElement.appendTo = function appendTo(el) {
+    el.appendChild(element);
+  };
+
+  return {
+    template,
+    context,
+    element: innerElement,
+  };
 });
