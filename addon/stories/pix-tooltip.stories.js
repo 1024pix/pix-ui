54763c84b77d95e671bebd7ca0aca028f4a52ecd
Krysthalia <ang.schall@gmail.com>
2020-05-27 17:53:32 +0200

    Add a story for tooltip

--- addon/stories/pix-tooltip.stories.js
+++ addon/stories/pix-tooltip.stories.js
@@ -43,3 +43,36 @@ export const tooltip = () => {
     `,
   }
 };
+
+tooltip.story = {
+  parameters: {
+    notes: {
+      markdown: `
+      # Tooltip
+      Une infobulle qui s'affiche au survol d'un élément.
+      
+      ___
+
+      # Usage
+      ~~~javascript
+      <PixTooltip
+        @text='Texte d'information à afficher'
+        @position='bottom'
+        >
+          // ce que je veux ici, par exemple un bouton : 
+          <button>Mon élément à survoler</button>
+      </PixTooltip>
+      ~~~
+
+      ___
+
+      # Props
+      
+| Nom           | Type          | Valeurs possibles |
+| ------------- |:-------------:|------------------:|
+| text          | string        |            -             |
+| position      | string        | "top", "right", "bottom", "left"     |
+    `,
+    },
+  },
+};
