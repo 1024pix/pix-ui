100df79919db70b8b22947895901ae22003cf74a
Krysthalia <ang.schall@gmail.com>
2020-05-29 15:55:57 +0200

    Add the possibility to show tooltip inline

--- addon/components/pix-tooltip.js
+++ addon/components/pix-tooltip.js
@@ -5,5 +5,9 @@ export default Component.extend({
   getPosition: computed('position', function() {
     const correctsPosition = ['top', 'right', 'bottom', 'left'];
     return correctsPosition.includes(this.position) ? this.position : 'top';
+  }),
+  displayInline: computed('inline', function() {
+    const inlineInString = this.inline ? this.inline.toString() : '';
+    return inlineInString == 'true' ? 'pix-tooltip__content--no-wrap' : '';
   })
 });
