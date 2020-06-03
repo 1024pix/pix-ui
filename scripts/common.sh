673820541652e99a0550cf591f4acc170cc31f0f
Pix <pix@>
2020-06-03 11:25:41 +0200

    Add deploy script

---
+++ scripts/common.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Set colors
+RESET_COLOR="\033[0m"
+BOLD="\033[1m"
+OFFBOLD="\033[21m"
+
+# Colors (bold)
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+CYAN="\033[1;36m"
+
+function clone_repository_and_move_inside {
+  REPOSITORY_FOLDER=$(mktemp -d)
+  echo "Created temporary directory ${REPOSITORY_FOLDER}"
+
+  git clone "https://github.com/1024pix/pix-ui.git" "${REPOSITORY_FOLDER}"
+  echo "Cloned repository to temporary directory"
+
+  cd "${REPOSITORY_FOLDER}" || exit 1
+  echo "Moved to repository folder"
+}
+
+function configure_git_user_information {
+  git config user.name "${GIT_USER_NAME}"
+  git config user.email "${GIT_USER_EMAIL}"
+  echo "Set Git user information"
+}
