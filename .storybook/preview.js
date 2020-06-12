3d4eed3b40366c28b8eaec507915f75547239eb7
melanieb <melanie.boudard@gmail.com>
2020-06-12 15:05:22 +0200

    Add a11y check in one story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,10 @@
-import { addParameters } from '@storybook/ember';
+import { addParameters, addDecorator } from '@storybook/ember';
 import storybookCustomTheme from './storybook-custom-theme';
+import { checkA11y } from '@storybook/addon-a11y';
 
 addParameters({
   options: {
     theme: storybookCustomTheme,
   },
 });
+addDecorator(checkA11y);
