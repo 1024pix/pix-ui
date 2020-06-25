9b16fb33d95f7ecaeff3819dea48f6c0bfc742ff
Benjamin Petetot <bpetetot@gmail.com>
2020-06-25 15:32:47 +0200

    Write button story

--- addon/stories/pix-button.stories.js
+++ addon/stories/pix-button.stories.js
@@ -3,32 +3,29 @@ import {hbs} from 'ember-cli-htmlbars';
 export default {title: 'Button'};
 
 const canvasContent = hbs`
+  <h3>Primary buttons</h3>
+  <PixButton @onClick={{fn onClick}}>Primary</PixButton>
+  <PixButton @rounded={{true}} @color="green" @onClick={{fn onClick}}>Primary</PixButton>
 
-  <h3>Default buttons</h3>
+  <h3>Secondary button</h3>
+  <PixButton @color="grey" @onClick={{fn onClick}}>Secondary</PixButton>
+
+  <h3>Tertiary button</h3>
+  <PixButton @type="outlined" @color="grey" @onClick={{fn onClick}}>Tertiary</PixButton>
+
+  <h3>Button sizes</h3>
   <PixButton @onClick={{fn onClick}}>Default</PixButton>
+  <PixButton @onClick={{fn onClick}} @size="large">Default</PixButton>
+
+  <h3>Colors</h3>
+  <PixButton @onClick={{fn onClick}}>Blue</PixButton>
   <PixButton @onClick={{fn onClick}} @color="green">Green</PixButton>
   <PixButton @onClick={{fn onClick}} @color="red">Red</PixButton>
   <PixButton @onClick={{fn onClick}} @color="yellow">Yellow</PixButton>
   <PixButton @onClick={{fn onClick}} @color="grey">Grey</PixButton>
-  <PixButton @onClick={{fn onClick}} @color="grey" disabled>Disabled</PixButton>
 
-  <h3>Outlined buttons</h3>
-  <PixButton @onClick={{fn onClick}} @type="outlined">Default</PixButton>
-  <PixButton @onClick={{fn onClick}} @type="outlined" @color="green">Green</PixButton>
-  <PixButton @onClick={{fn onClick}} @type="outlined" @color="red">Red</PixButton>
-  <PixButton @onClick={{fn onClick}} @type="outlined" @color="yellow">Yellow</PixButton>
-  <PixButton @onClick={{fn onClick}} @type="outlined" @color="grey">Grey</PixButton>
-
-  <h3>Rounded buttons</h3>
-  <PixButton @onClick={{fn onClick}} @rounded={{true}}>Default</PixButton>
-  <PixButton @onClick={{fn onClick}} @rounded={{true}} @color="green">Green</PixButton>
-  <PixButton @onClick={{fn onClick}} @rounded={{true}} @color="red">Red</PixButton>
-  <PixButton @onClick={{fn onClick}} @rounded={{true}} @color="yellow">Yellow</PixButton>
-  <PixButton @onClick={{fn onClick}} @rounded={{true}} @color="grey">Grey</PixButton>
-
-  <h3>Button sizes</h3>
-  <PixButton @onClick={{fn onClick}}>Default</PixButton>
-  <PixButton @onClick={{fn onClick}} @size="large">Large</PixButton>
+  <h3>Button disabled</h3>
+  <PixButton disabled @onClick={{fn onClick}}>Primary</PixButton>
 `;
 
 const markdown = `
@@ -46,10 +43,10 @@ Les boutons utilisés un peu partout sur la plateforme, notamment dans les formu
  @rounded="true|false"
  @size="medium|large"
  @onClick="function"
- @disabled="true|false"
  @loading="true|false"
+ disabled="true|false"
 >
-    Mon Bouton
+  Disabled
 </PixButton>
 
 ~~~
@@ -58,13 +55,13 @@ Les boutons utilisés un peu partout sur la plateforme, notamment dans les formu
 
 | Nom      |   Type   |       Valeurs possibles        | Par défaut | Optionnel |
 | -------- | :------: | :----------------------------: | :--------: | --------: |
-| type     |  string  |        solid, outlined         |   solid    |       oui |
-| color    |  string  | blue, red, yellow, green, grey |    blue    |       oui |
-| rounded  | boolean  |          true, false           |   false    |       oui |
-| size     |  string  |         medium, large          |   medium   |       oui |
-| onClick  | function |     l'action à déclencher      |     -      |       non |
-| disabled | boolean  |          true, false           |   false    |       oui |
-| loading  | boolean  |          true, false           |   false    |       oui |
+| @type     |  string  |        solid, outlined         |   solid    |       oui |
+| @color    |  string  | blue, red, yellow, green, grey |    blue    |       oui |
+| @rounded  | boolean  |          true, false           |   false    |       oui |
+| @size     |  string  |         medium, large          |   medium   |       oui |
+| @onClick  | function |     l'action à déclencher      |     -      |       non |
+| @loading  | boolean  |          true, false           |   false    |       oui |
+| disabled  | boolean  |          true, false           |   false    |       oui |
 `;
 
 export const button = () => {
