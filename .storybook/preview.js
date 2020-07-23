1d9fe1c1d2a36d5ff2d103b27179f06acb22a658
Krysthalia <ang.schall@gmail.com>
2020-07-23 10:39:38 +0200

    Fix 'checkA11y has been renamed withA11y'

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
 import { addParameters, addDecorator } from '@storybook/ember';
 import storybookCustomTheme from './storybook-custom-theme';
-import { checkA11y } from '@storybook/addon-a11y';
+import { withA11y } from '@storybook/addon-a11y';
 
 addParameters({
   options: {
     theme: storybookCustomTheme,
   },
 });
-addDecorator(checkA11y);
+addDecorator(withA11y);
