2a4bc0b3f2fbf5ba9356f3bc9f7fd460b8e582b5
Krysthalia <ang.schall@gmail.com>
2020-07-30 10:28:24 +0200

    Update addon.scss to import new style

--- blueprints/pix-component/index.js
+++ blueprints/pix-component/index.js
@@ -1,5 +1,28 @@
 'use strict';
+const stringUtils = require('ember-cli-string-utils');
+const insertIntoFile = require('../../lib/utilities/insert-into-file');
+const EOL = require('os').EOL;
 
 module.exports = {
   description: 'To create a Pix component with default files',
+
+  locals(options) {
+    const componentName = options.entity.name;
+    const dasherizedName = 'pix-' + stringUtils.dasherize(componentName);
+
+    importNewStyleInGlobalStyle(dasherizedName);
+
+    return {};
+  },
 };
+
+async function importNewStyleInGlobalStyle(dasherizedName) {
+  const importName = '@import \'' + dasherizedName + '\';';
+
+  await insertIntoFile(
+    'addon/styles/addon.scss',
+    importName,
+    { before: '\nhtml {' + EOL }
+  )
+  console.log('  update addon/styles/addon.scss');
+}
