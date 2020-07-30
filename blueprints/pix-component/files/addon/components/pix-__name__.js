07d1e53ffbc54a921695ee1bd7c01380d20a944f
Krysthalia <ang.schall@gmail.com>
2020-07-30 10:28:16 +0200

    Add new files with customblueprint

---
+++ blueprints/pix-component/files/addon/components/pix-__name__.js
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class Pix<%= classifiedModuleName %> extends Component {
+  text = 'pix-<%= dasherizedModuleName %>';
+}
---
+++ blueprints/pix-component/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  description: 'To create a Pix component with default files',
+};
