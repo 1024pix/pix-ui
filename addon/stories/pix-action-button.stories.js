7e20342e3d0bc80b8edb8d8e8c118119848d0262
celineung <celine.ung@zenika.com>
2020-09-15 16:23:55 +0200

    Update action-button story

--- addon/stories/pix-action-button.stories.js
+++ addon/stories/pix-action-button.stories.js
@@ -4,27 +4,35 @@ import centered from '@storybook/addon-centered/ember';
 export default { title: 'ActionButton' };
 
 const canvasContent = hbs`
-  <PixActionButton>
-  </PixActionButton>
+  <h3>Par défaut:</h3>
+  <PixActionButton />
+  <h3>Avec l'icon 'times':</h3>
+  <PixActionButton @icon='times' />
 `;
 
 const markdown = `
 # ActionButton
-TODO
+Le ActionButton permet de créer un bouton contenant une icône font-awesome.
 
 ## Usage
 
+Par défaut:
+
 ~~~javascript
-<PixActionButton>
-  // TODO
-</PixActionButton>
+<PixActionButton />
+~~~
+
+Avec paramètre:
+
+~~~javascript
+<PixActionButton @icon='times' />
 ~~~
 
 ## Props
 
 | Nom           | Type          |  Valeurs possibles  | Par défaut | Optionnel |
 | ------------- |:-------------:|:-------------------:|:----------:|----------:|
-| | | | | |
+|icon|string|les icons de font-awesome|'plus'|oui|
 
 `
 ;
