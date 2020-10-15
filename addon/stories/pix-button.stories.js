c63b10a279f0065327fcc976a683bc8b0ce23f25
melanieb <melanie.boudard@gmail.com>
2020-10-15 14:19:52 +0200

    Simplify css

--- addon/stories/pix-button.stories.js
+++ addon/stories/pix-button.stories.js
@@ -7,22 +7,6 @@ const canvasContent = {
   template: hbs`
     <h2> Bouton avec loader blanc et une action qui se résout</h2>
     <PixButton
-    style="
-      width: 280px;
-      padding: 14px 20px;
-      font-family: Arial, sans-serif;
-      font-size: 1rem;
-      font-weight: 500;
-      letter-spacing: 0.012rem;
-      text-align: center;
-      border-radius: 5px;
-      background-color: #3D68FF;
-      color: #FFFFFF;
-      cursor: pointer;
-      border: none;
-      outline: none;
-      transition: background-color 0.2s ease;
-      position: relative;"
     @action={{action onClick}}
     @loading-color='white'
     >
@@ -30,22 +14,6 @@ const canvasContent = {
 </PixButton>
  <h2> Bouton avec loader gris et une action qui se ne résout pas</h2>
     <PixButton
-    style="
-      width: 280px;
-      padding: 14px 20px;
-      font-family: Arial, sans-serif;
-      font-size: 1rem;
-      font-weight: 500;
-      letter-spacing: 0.012rem;
-      text-align: center;
-      border-radius: 5px;
-      background-color: #FFBE00;
-      color: #253858;
-      cursor: pointer;
-      border: none;
-      outline: none;
-      transition: background-color 0.2s ease;
-      position: relative;"
     @action={{action onClickFailed}}
     @loading-color='grey'
     >
