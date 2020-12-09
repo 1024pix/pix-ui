98c04f6cbfb9313c3274127bf98c4728bd78351a
celineung <celine.ung@zenika.com>
2020-12-09 16:42:32 +0100

    Add knobs dependency

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
-  addons: ['@storybook/addon-notes/register-panel', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-notes/register-panel',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
+  ],
   stories: ['../addon/**/*.stories.js'],
 };
