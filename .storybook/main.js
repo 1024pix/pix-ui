4b606a845997188cd839565e2eae3995c1459ba9
Benjamin Petetot <bpetetot@gmail.com>
2020-12-11 11:01:06 +0100

    Configure docs addon for Storybook
    In preview.js, viewDocs and previewTabs set Docs as default tab.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,9 @@
 module.exports = {
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-notes/register-panel',
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
   ],
-  stories: ['../addon/**/*.stories.js'],
+  stories: ['../addon/**/*.stories.@(js|mdx)'],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,11 @@ export const parameters = {
   a11y: {
     element: '#root',
   },
+  viewMode: 'docs',
+  docs: {
+    iframeHeight: 400,
+  },
+  previewTabs: { 
+    'storybook/docs/panel': { index: -1 },
+  },
 };
