d0ffa809f6b0ea224d31ff49a1c20d68e7c9632e
celineung <celine.ung@zenika.com>
2020-12-15 17:57:17 +0100

    Update PixActionButton files to use mdx

--- addon/stories/pix-action-button.stories.js
+++ addon/stories/pix-action-button.stories.js
@@ -1,48 +1,27 @@
 import { hbs } from 'ember-cli-htmlbars';
-import centered from '@storybook/addon-centered/ember';
 
-export default { title: 'Basics/Action Button' };
-
-const canvasContent = hbs`
-  <h3>Par défaut:</h3>
-  <PixActionButton />
-  <h3>Avec l'icon 'times':</h3>
-  <PixActionButton @icon='times' />
-`;
-
-const markdown = `
-# ActionButton
-Le ActionButton permet de créer un bouton contenant une icône font-awesome.
-
-## Usage
-
-Par défaut:
-
-~~~javascript
-<PixActionButton />
-~~~
-
-Avec paramètre:
-
-~~~javascript
-<PixActionButton @icon='times' />
-~~~
-
-## Props
-
-| Nom           | Type          |  Valeurs possibles  | Par défaut | Optionnel |
-| ------------- |:-------------:|:-------------------:|:----------:|----------:|
-|icon|string|les icons de font-awesome|'plus'|oui|
-|triggerAction|function|-|-|oui|
-
-`
-;
-
-export const actionButton = () => {
+export const actionButton = (args) => {
   return {
-    template: canvasContent,
-  }
+    template: hbs`
+      <PixActionButton
+        @icon={{icon}}
+        @triggerAction={{triggerAction}}
+      />
+    `,
+    context: args,
+  };
 };
 
-actionButton.parameters = { notes: { markdown } };
-actionButton.decorators = [centered];
+export const argTypes = {
+  icon: {
+    name: 'icon',
+    description: 'icon fontawesome',
+    type: { name: 'string', required: false },
+    defaultValue: 'times',
+  },
+  triggerAction: {
+    name: 'triggerAction',
+    description: 'fonction à appeler au clic du bouton',
+    type: { required: false },
+  },
+};
