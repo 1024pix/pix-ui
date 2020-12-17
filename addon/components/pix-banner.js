7d145c7ea9c747992f5a6178a435838e3dd94c44
celineung <celine.ung@zenika.com>
2020-12-17 13:57:24 +0100

    Update PixBanner to use mdx

--- addon/components/pix-banner.js
+++ addon/components/pix-banner.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 
-export default class PixBannerComponent extends Component {
+export default class PixBanner extends Component {
   get displayAction() {
     return this.args.actionLabel && this.args.actionUrl;
   }
