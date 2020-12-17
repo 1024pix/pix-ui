552bc3fb8b9676b1da5ff73a92f8e68d54613319
celineung <celine.ung@zenika.com>
2020-12-17 13:57:24 +0100

    Update PixBanner to use mdx

--- addon/stories/pix-banner.stories.js
+++ addon/stories/pix-banner.stories.js
@@ -1,58 +1,28 @@
 import { hbs } from 'ember-cli-htmlbars';
 
-export default { title: 'Notification/Banner' };
-
-const canvasContent = hbs`
-<h3>Message important sans action</h3>
-<PixBanner>
-  Bienvenue sur PixOrga !
-</PixBanner>
-
-<h3>Message important avec lien externe</h3>
-<PixBanner
-  @actionLabel='Ajouter des élèves'
-  @actionUrl='https://orga.pix.fr/eleves'
->
-  La liste des élèves est vide, ajouter des élèves pour commencer
-</PixBanner>
-<h3>Message important avec lien-interne</h3>
-<PixBanner
-  @actionLabel='Liste des campagnes'
-  @actionUrl='authenticated.campaigns'
->
-  Parcours de rentrée 2020 : les codes sont disponibles dans l'onglet campagne. N’oubliez pas de les diffuser aux élèves avant la Toussaint.
-</PixBanner>
-`;
-
-const markdown = `
-# Banner
-
-Une \`Banner\` permet de mettre en avant une information importante.
-
-> Il est possible de surcharger le style d'une \`<PixBanner>\` via l'attribut \`class\` ainsi que de passer n'importe quel attribut sur sa \`div\` wrapper (par exemple, un \`aria-label\`)
-
-## Usage
-
-~~~javascript
-<PixBanner @actionLabel='Ajouter des élèves' @actionUrl='https://orga.pix.fr/eleves' aria-label="ma bannière">
-  // ici ajouter le contenu de la bannière
-</PixBanner>
-~~~
-
-## Props
-
-| Nom           | Type          | Valeurs possibles     | Par défaut | Optionnel |
-| ------------- |:-------------:|:---------------------:|:----------:|----------:|
-| actionLabel   | string        |                       | null       | oui       |
-| actionUrl     | url           |                       | null       | oui       |
-`
-;
-
-export const banner = () => {
+export const banner = (args) => {
   return {
-    template: canvasContent,
-  }
+    template: hbs`
+      <PixBanner
+        @actionLabel={{actionLabel}}
+        @actionUrl={{actionUrl}}
+      >Parcours de rentrée 2020 : les codes sont disponibles dans l'onglet campagne. N’oubliez pas de les diffuser aux élèves avant la Toussaint.</PixBanner>
+    `,
+    context: args,
+  };
 };
 
-banner.parameters = { notes: { markdown } };
-
+export const argTypes = {
+  actionLabel: {
+    name: 'action label',
+    description: 'Nom de l‘action',
+    type: { name: 'string', required: false },
+    defaultValue: null,
+  },
+  actionUrl: {
+    name: 'action url',
+    description: 'Action link',
+    type: { name: 'string', required: false },
+    defaultValue: null,
+  }
+};
