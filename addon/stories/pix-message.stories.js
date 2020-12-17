d6f6a2518e9ea85c32859d7068aff298c70a4177
celineung <celine.ung@zenika.com>
2020-12-17 16:32:04 +0100

    Update PixMessage to MDX

--- addon/stories/pix-message.stories.js
+++ addon/stories/pix-message.stories.js
@@ -1,58 +1,22 @@
 import { hbs } from 'ember-cli-htmlbars';
-import centered from '@storybook/addon-centered/ember';
 
-export default { title: 'Notification/Message' };
-
-const canvasContent = hbs`
-<PixMessage @type='info'>
-  Ceci est un message à caractère informatif.
-</PixMessage>
-
-<br><br>
-
-<PixMessage @type='alert'>
-  <strong>Attention</strong> à la marche ! 
-</PixMessage>
-
-<br><br>
-
-<PixMessage @type='success'>
-  This is a success message.
-</PixMessage>
-
-<br><br>
-
-<PixMessage @type='warning'>
-  This is a warning.
-</PixMessage>
-
-`;
-
-const markdown = `
-# Message
-Un bandeau d'information, par défaut de type info. 
-
-## Usage
-
-~~~javascript
-
-<PixMessage @type='info'>
-  // Ici les informations à afficher dans le bandeau.
-</PixMessage>
-~~~
-
-## Props
-
-| Nom           | Type          | Valeurs possibles | Par défaut | Optionnel |
-| ------------- |:-------------:|:-----------------:|:----------:|----------:|
-| type          | string        |         ["info", "success", "warning", "alert"]         |     "info"      | oui |
-`;
-
-export const message = () => {
+export const stars = (args) => {
   return {
-    template: canvasContent,
-  }
+    template: hbs`
+      <PixMessage
+        @type={{type}}
+      >Ceci est un message</PixMessage>
+    `,
+    context: args,
+  };
 };
 
-message.parameters = { notes: { markdown } };
-message.decorators = [centered];
+export const argTypes = {
+  type: {
+    name: 'type',
+    description: 'Type du message',
+    type: { name: 'string', required: false },
+    defaultValue: 'info',
+    control: { type: 'select', options: ['info', 'success', 'warning', 'alert'] },
+  },
+}
