0109d5cc43cbcc10b40ce152c823b4643f54231f
celineung <celine.ung@zenika.com>
2020-12-18 16:55:02 +0100

    Update PixButton to use MDX

--- addon/stories/pix-button.stories.js
+++ addon/stories/pix-button.stories.js
@@ -1,73 +1,47 @@
 import { hbs } from 'ember-cli-htmlbars';
-import centered from '@storybook/addon-centered/ember';
 
-export default { title: 'Basics/Button' };
+export const button = (args) => {
+  return {
+    template: hbs`
+      <PixButton
+        @triggerAction={{action triggerAction}}
+        @loading-color={{loadingColor}}
+        @type={{type}}>
+        Nom du bouton
+      </PixButton>
+    `,
+    context: {
+      ...args,
+    },
+  };
+};
 
-const canvasContent = {
-  template: hbs`
-    <h2> Bouton avec loader blanc et une action qui se résout</h2>
-    <PixButton
-    @triggerAction={{action onClick}}
-    @loading-color='white'
-    >
-    Cliquez pour valider !
-</PixButton>
- <h2> Bouton avec loader gris et une action qui se ne résout pas</h2>
-    <PixButton
-    @triggerAction={{action onClickFailed}}
-    @loading-color='grey'
-    >
-    Cliquez pour valider !
-</PixButton>`,
-  context: {
-    onClick: async () => {
-      const promise = new Promise((resolve) => {
+export const argTypes = {
+  triggerAction: {
+    name: 'action',
+    description: 'fonction à appeler en cas de clic',
+    type: { required: true },
+    defaultValue: () => {
+      return new Promise((resolve) => {
         setTimeout(() => {
           console.log('OK !')
           resolve();
         }, 2000);
-      });
-      return promise
+      })
     },
-    onClickFailed: async () => {
-      const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          console.log('Marche pas :(')
-          reject();
-        }, 2000);
-      });
-      return promise
-    }
-
-}
-}
-
-const markdown = `
-# Button
-Ce composant est un bouton simple qui empêche les clics multiples.
-
-## Usage
-
-~~~javascript
-<PixButton @triggerAction={{action yourAction}} @loading-color='grey' @type='button'>
-  Nom du bouton
-</PixButton>
-~~~
-
-## Props
-
-| Nom           | Type          |  Valeurs possibles  | Par défaut | Optionnel |
-| ------------- |:-------------:|:-------------------:|:----------:|----------:|
-| triggerAction | action | | | Non |
-| loading-color | string | white/grey | white | Oui |
-| type | string | button/submit | button | Oui |
-
-`
-;
-
-export const button = () => {
-  return canvasContent
+  },
+  loadingColor: {
+    name: 'couleur de chargement',
+    description: '',
+    type: { name: 'string', required: false },
+    defaultValue: 'Message',
+    control: { type: 'select', options: ['yellow', 'blue', 'grey'] },
+  },
+  type: {
+    name: 'type',
+    description: 'type du bouton',
+    type: { required: false },
+    defaultValue: 'button',
+    control: { type: 'select', options: ['button', 'submit'] },
+  },
 };
-
-button.parameters = { notes: { markdown } };
-button.decorators = [centered];
