149572adaa8863431406b0bc48d46c2ec35f66aa
Karam Haytham <karam.haytham.mohammed@octo.com>
2021-01-27 19:40:44 +0100

    added "details" attribute to the filter banner component

--- addon/components/pix-filter-banner.js
+++ addon/components/pix-filter-banner.js
@@ -4,4 +4,7 @@ export default class PixFilterBanner extends Component {
   get displayTitle() {
     return Boolean(this.args.title);
   }
+  get displayDetails() {
+    return Boolean(this.args.details);
+  }
 }
