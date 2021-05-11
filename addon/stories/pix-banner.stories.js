2a3b91a0a51363276c2de35ebaf24b55aaae7a65
yaelle <ymarsiac@octo.com>
2021-05-11 13:43:50 +0200

    Add type to  pix Banner and change css

--- addon/stories/pix-banner.stories.js
+++ addon/stories/pix-banner.stories.js
@@ -12,8 +12,8 @@ export const bannerWithExternalLink = (args) => {
   return {
     template: hbs`
       <PixBanner
-        @actionLabel={{actionLabel}}
-        @actionUrl={{actionUrl}}
+        @actionLabel= "Voir le nouveau site"
+        @actionUrl= "www.test.fr/"
       >Parcours de rentrée 2020 : les codes sont disponibles dans l'onglet campagne. N’oubliez pas de les diffuser aux élèves avant la Toussaint.</PixBanner>
     `,
     context: args,
@@ -24,8 +24,42 @@ export const bannerWithInternalLink = (args) => {
   return {
     template: hbs`
       <PixBanner
-        @actionLabel={{actionLabel}}
-        @actionUrl={{actionUrl}}
+        @actionLabel= "Voir la liste des participants"
+        @actionUrl= "campaign.list"
+      >Parcours de rentrée 2020 : les codes sont disponibles dans l'onglet campagne. N’oubliez pas de les diffuser aux élèves avant la Toussaint.</PixBanner>
+    `,
+    context: args,
+  };
+};
+
+export const warningBanner = (args) => {
+  return {
+    template: hbs`
+      <PixBanner
+        @type="warning"
+      >Parcours de rentrée 2020 : les codes sont disponibles dans l'onglet campagne. N’oubliez pas de les diffuser aux élèves avant la Toussaint.</PixBanner>
+    `,
+    context: args,
+  };
+};
+
+export const errorBanner = (args) => {
+  return {
+    template: hbs`
+      <PixBanner
+        @type="error"
+      >Parcours de rentrée 2020 : les codes sont disponibles dans l'onglet campagne. N’oubliez pas de les diffuser aux élèves avant la Toussaint.</PixBanner>
+    `,
+    context: args,
+  };
+};
+
+export const communicationBanner = (args) => {
+  return {
+    template: hbs`
+      <PixBanner
+        @type="communication"
+        @color="blue"
       >Parcours de rentrée 2020 : les codes sont disponibles dans l'onglet campagne. N’oubliez pas de les diffuser aux élèves avant la Toussaint.</PixBanner>
     `,
     context: args,
@@ -44,5 +78,19 @@ export const argsTypes = {
     description: 'Action link',
     type: { name: 'string', required: false },
     defaultValue: null,
+  },
+  type: {
+    name: 'type',
+    description: 'Définit le type de bannière',
+    type: { name: 'string', required: false },
+    defaultValue: 'information',
+    control: { type: 'select', options: ['information', 'warning', 'error'] },
+  },
+  color: {
+    name: 'color',
+    description: 'Détermine la couleur de la bannière de communication',
+    type: { name: 'string', required: false },
+    defaultValue: 'blue',
+    control: { type: 'select', options: ['blue', 'green', 'yellow', 'purple'] },
   }
 };
---
+++ tests/integration/components/pix-banner-test.js
@@ -0,0 +1,97 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | Pix Banner', function(hooks) {
+  setupRenderingTest(hooks);
+
+  const COMPONENT_SELECTOR = '.pix-banner';
+
+  test('it renders the default PixBanner', async function(assert) {
+    // when
+    await render(hbs`
+      <PixBanner>
+        Mon texte
+      </PixBanner>
+    `);
+
+    // then
+    const componentElement = this.element.querySelector(COMPONENT_SELECTOR);
+    assert.equal(componentElement.textContent.trim(), 'Mon texte');
+    assert.equal(componentElement.classList.toString().trim(), 'pix-banner pix-banner-information');
+  });
+
+  test('it renders the PixBanner with type warning', async function(assert) {
+    // when
+    await render(hbs`
+      <PixBanner @type='warning'>
+        Mon texte
+      </PixBanner>
+    `);
+
+    // then
+    const componentElement = this.element.querySelector(COMPONENT_SELECTOR);
+    assert.equal(componentElement.textContent.trim(), 'Mon texte');
+    assert.equal(componentElement.classList.toString().trim(), 'pix-banner pix-banner-warning');
+  });
+
+  test('it renders the PixBanner with type error', async function(assert) {
+    // when
+    await render(hbs`
+      <PixBanner @type='error'>
+        Mon texte
+      </PixBanner>
+    `);
+
+    // then
+    const componentElement = this.element.querySelector(COMPONENT_SELECTOR);
+    assert.equal(componentElement.textContent.trim(), 'Mon texte');
+    assert.equal(componentElement.classList.toString().trim(), 'pix-banner pix-banner-error');
+  });
+
+  test('it renders the PixBanner with external url', async function(assert) {
+    // given
+    this.set('actionUrl', 'www.test.fr/');
+    this.set('actionLabel', 'Explorer');
+
+    //when
+    await render(hbs`
+      <PixBanner @actionUrl={{this.actionUrl}} @actionLabel={{this.actionLabel}} />
+    `);
+
+    // then
+    assert.dom('a').exists();
+    assert.equal(this.element.querySelector('a').getAttribute('href'), 'www.test.fr/');
+  });
+
+  test('it renders the PixBanner with internal link', async function(assert) {
+    // given
+  
+    this.set('actionUrl', 'campaign.participants');
+    this.set('actionLabel', 'Explorer');
+
+    //when
+    await render(hbs`
+      <PixBanner @actionUrl={{this.actionUrl}} @actionLabel={{this.actionLabel}} />
+    `);
+
+    // then
+    assert.dom('a').exists();
+  });
+
+  test('it renders the PixBanner communication', async function(assert) {
+    // given
+  
+    this.set('color', 'yellow');
+    this.set('type', 'communication');
+
+    //when
+    await render(hbs`
+      <PixBanner @color={{this.color}} @type={{type}} />
+    `);
+
+    // then
+    assert.dom('.pix-banner-communication--yellow').exists();
+  });
+});
