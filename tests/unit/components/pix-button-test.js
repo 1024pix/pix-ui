7a611301c1a3f6b9116601505f3f52b7468e0efe
Pierre TOP (TOPI) <ptop@octo.com>
2021-06-15 16:14:57 +0200

    Allow no trigger action for pix button

--- tests/unit/components/pix-button-test.js
+++ tests/unit/components/pix-button-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 import createGlimmerComponent from '../../helpers/create-glimmer-component';
 
 
@@ -18,4 +19,43 @@ module('Unit | Component | pix-button', function(hooks) {
       expectedError
     );
   });
+
+  module('#enableTriggerAction', function(){
+
+    test('it should return true if button type is button', function(assert){
+      // given
+      const componentParams = { type: 'button' };
+      const component = createGlimmerComponent('component:pix-button', componentParams);
+
+      // when
+      const result = component.enableTriggerAction;
+
+      // then
+      assert.equal(result, true);
+    })
+
+    test('it should return true if button type is submit and triggerAction is defined', function(assert){
+      // given
+      const componentParams = { type: 'submit', triggerAction: sinon.stub() };
+      const component = createGlimmerComponent('component:pix-button', componentParams);
+
+      // when
+      const result = component.enableTriggerAction;
+
+      // then
+      assert.equal(result, true);
+    })
+
+    test('it should return false if button type is submit and triggerAction is not defined', function(assert){
+      // given
+      const componentParams = { type: 'submit' };
+      const component = createGlimmerComponent('component:pix-button', componentParams);
+
+      // when
+      const result = component.enableTriggerAction;
+
+      // then
+      assert.equal(result, false);
+    })
+  })
 });
