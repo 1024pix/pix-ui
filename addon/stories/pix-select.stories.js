8fd06c72e63ae0c59f312d12c8e1a01df2be7814
Anne-G <ang.schall@gmail.com>
2021-08-12 14:17:08 +0200

    refacto pix select story

--- addon/stories/pix-select.stories.js
+++ addon/stories/pix-select.stories.js
@@ -3,9 +3,6 @@ import { hbs } from 'ember-cli-htmlbars';
 export const select = (args) => {
   return {
     template: hbs`
-      <h3 style="margin: 10px;">
-        <label for="default-pix-select">PixSelect par défaut: </label>
-      </h3>
       <PixSelect
         id="default-pix-select"
         @options={{options}}
@@ -13,6 +10,8 @@ export const select = (args) => {
         @selectedOption={{selectedOption}}
         @emptyOptionLabel={{emptyOptionLabel}}
         @emptyOptionNotSelectable={{emptyOptionNotSelectable}}
+        @isSearchable={{isSearchable}}
+        @isValidationActive={{isValidationActive}}
       />
     `,
     context: args,
@@ -22,39 +21,40 @@ export const select = (args) => {
 export const searchableSelect = (args) => {
   return {
     template: hbs`
-      <h3 style="margin: 10px;">
-        <label for="searchable-pix-select" style="margin: 10px">PixSelect cherchable</label>
-      </h3>
-        <PixSelect
-          id="searchable-pix-select"
-          @options={{options}}
-          @onChange={{onChange}}
-          @selectedOption={{selectedOption}}
-          @emptyOptionLabel={{emptyOptionLabel}}
-          @emptyOptionNotSelectable={{false}}
-          @isSearchable={{true}}
-          @isValidationActive={{true}}
-          placeholder='Fraises, Mangues...'
-        />
+      <PixSelect
+        id="searchable-pix-select"
+        @options={{options}}
+        @onChange={{onChange}}
+        @selectedOption={{selectedOption}}
+        @emptyOptionLabel={{emptyOptionLabel}}
+        @emptyOptionNotSelectable={{emptyOptionNotSelectable}}
+        @isSearchable={{isSearchable}}
+        @isValidationActive={{isValidationActive}}
+        placeholder={{this.placeholder}}
+      />
     `,
-    context: {
-      ...args,
-      options: [
-        { value: 'Figues', label: 'Figues' },
-        { value: 'Bananes', label: 'Bananes' },
-        { value: 'Noix', label: 'Noix' },
-        { value: 'Papayes', label: 'Papayes' },
-        { value: 'Fèves de chocolat', label: 'Fèves de chocolat' },
-        { value: 'Dattes', label: 'Dattes' },
-        { value: 'Mangues', label: 'Mangues' },
-        { value: 'Jujube', label: 'Jujube' },
-        { value: 'Avocat', label: 'Avocat' },
-        { value: 'Fraises', label: 'Fraises' },
-        { value: 'Kaki', label: 'Kaki' },
-      ]
-    }
+    context: args,
   };
 };
+searchableSelect.args = {
+  emptyOptionNotSelectable: false,
+  isSearchable: true,
+  isValidationActive: true,
+  placeholder: 'Fraises, Mangues...',
+  options: [
+    { value: 'Figues', label: 'Figues' },
+    { value: 'Bananes', label: 'Bananes' },
+    { value: 'Noix', label: 'Noix' },
+    { value: 'Papayes', label: 'Papayes' },
+    { value: 'Fèves de chocolat', label: 'Fèves de chocolat' },
+    { value: 'Dattes', label: 'Dattes' },
+    { value: 'Mangues', label: 'Mangues' },
+    { value: 'Jujube', label: 'Jujube' },
+    { value: 'Avocat', label: 'Avocat' },
+    { value: 'Fraises', label: 'Fraises' },
+    { value: 'Kaki', label: 'Kaki' },
+  ]
+}
 
 export const argTypes = {
   options: {
@@ -79,33 +79,38 @@ export const argTypes = {
     name: 'onChange',
     description: 'Fonction à appeler si une option est sélectionnée',
     type: { required: true },
-    defaultValue: () => { },
+    defaultValue: () => console.log('change'),
+    control: { disable: true },
   },
   selectedOption: {
     name: 'selectedOption',
     description: 'Option sélectionnée',
+    control: { type: 'string' },
     type: { name: 'string', required: false },
   },
   emptyOptionLabel: {
     name: 'emptyOptionLabel',
     description: 'Texte à afficher si aucune option n‘est sélectionnée',
+    control: { type: 'string' },
     type: { name: 'string', required: false },
   },
   isSearchable: {
     name: 'isSearchable',
     description: 'Rend le champ cherchable',
+    control: { type: 'boolean' },
     type: { name: 'boolean', required: false },
   },
   isValidationActive: {
     name: 'isValidationActive',
     description:
       'Rend la bordure du champ vert au focus si la valeur de recherche match une option (c\'est à dire si l\'utilisateur a selectionné une option valable',
+    control: { type: 'boolean' },
     type: { name: "boolean", required: false },
   },
   emptyOptionNotSelectable: {
     name: 'emptyOptionNotSelectable',
     description: 'Rend le premier champ qui est vide non visible une fois sélectionné',
+    control: { type: 'boolean' },
     type: { name: 'boolean', required: false },
   },
-
 };
