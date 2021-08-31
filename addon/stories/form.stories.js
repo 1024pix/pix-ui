ab49e5db2af87f7c09392731b10c6ecb6219e681
Anne-G <ang.schall@gmail.com>
2021-08-31 12:14:37 +0200

    add form example documentation to storybook

---
+++ addon/stories/form.stories.js
@@ -0,0 +1,56 @@
+import { hbs } from 'ember-cli-htmlbars';
+import { action } from '@storybook/addon-actions';
+
+export const form = (args) => {
+  return {
+    template: hbs`
+      <PixInput @id='firstName' @label='Prénom' />
+      <br>
+
+      <PixMultiSelect
+        @title="A quel point aimez vous Pix-UI ?"
+        @id="form__pix-mutli-select"
+        @onSelect={{onSelect}}
+        @selected={{selected}}
+        @options={{options}} as |star|
+      >
+        <PixStars @count={{star.value}} @total={{star.total}} />
+      </PixMultiSelect>
+      <br><br>
+
+      <PixSelect
+        id="form__searchable-pix-select"
+        @options={{selectOptions}}
+        @isSearchable={{true}}
+        @isValidationActive={{true}}
+        placeholder='Fraises, Mangues...'
+      />
+      <br>
+
+      <PixTextarea @id="form__pix-textarea" @value="" @maxlength={{200}} />
+    `,
+    context: args,
+  };
+};
+form.args = {
+  selected: ['1','4'],
+  options: [
+    { value: '1', total: 3 },
+    { value: '2', total: 3 },
+    { value: '3', total: 3 },
+  ],
+  onSelect: action('onSelect'),
+  selectOptions: [
+    { value: 'Figues', label: 'Figues' },
+    { value: 'Bananes', label: 'Bananes' },
+    { value: 'Noix', label: 'Noix' },
+    { value: 'Papayes', label: 'Papayes' },
+    { value: 'Fèves de chocolat', label: 'Fèves de chocolat' },
+    { value: 'Dattes', label: 'Dattes' },
+    { value: 'Mangues', label: 'Mangues' },
+    { value: 'Jujube', label: 'Jujube' },
+    { value: 'Avocat', label: 'Avocat' },
+    { value: 'Fraises', label: 'Fraises' },
+    { value: 'Kaki', label: 'Kaki' },
+  ],
+}
