29f80a742bf4d7342eac061c2218dfe483cf872a
Benjamin Petetot <bpetetot@gmail.com>
2021-09-13 14:50:06 +0200

    Fix download same file twice with upload button

--- addon/components/pix-button-upload.js
+++ addon/components/pix-button-upload.js
@@ -1,15 +1,20 @@
 import PixButtonBase from './pix-button-base';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class PixButtonUpload extends PixButtonBase {
+  @tracked
+  files = [];
+
   get className() {
     return super.baseClassNames.join(' ');
   }
 
   @action
-  handleChange(e) {
-    if (e.target && e.target.files && e.target.files.length) {
-      this.args.onChange(e.target.files);
+  async handleChange(e) {
+    if (e.target?.files?.length) {
+      await this.args.onChange(e.target.files);
     }
+    this.files = [];
   }
 }
