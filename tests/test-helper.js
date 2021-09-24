4477d7d292e12d2e64b083e5e8efdc6bc2bebad7
Anne-G <ang.schall@gmail.com>
2021-09-24 16:21:12 +0200

    add contains test helper

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,11 @@ import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import QUnit from 'qunit';
+import { contains } from './helpers/contains';
 
 setApplication(Application.create(config.APP));
 
 start();
+
+QUnit.assert.contains = contains;
