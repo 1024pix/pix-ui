d656d5f9632c67dac45b5da9d0e5ce4b7143c7e8
Anne-G <ang.schall@gmail.com>
2021-09-27 14:41:45 +0200

    Fix required label for pix-select

--- addon/components/pix-select.js
+++ addon/components/pix-select.js
@@ -16,7 +16,7 @@ export default class PixSelect extends Component {
   
   get label() {
     const labelIsDefined = this.args.label && this.args.label.trim();
-    const idIsNotDefined = this.args.id && !this.args.id.trim();
+    const idIsNotDefined = !(this.args.id && this.args.id.trim());
 
     if (labelIsDefined && idIsNotDefined) {
       throw new Error('ERROR in PixSelect component, @id param is necessary when giving @label');
