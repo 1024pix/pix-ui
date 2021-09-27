384f90efec3652cfbbdc4b7422b5aa9caf0d609b
Anne-G <ang.schall@gmail.com>
2021-09-27 14:41:58 +0200

    Fix required label for pix-textarea

--- addon/components/pix-textarea.js
+++ addon/components/pix-textarea.js
@@ -9,7 +9,7 @@ export default class PixTextarea extends Component {
 
   get label() {
     const labelIsDefined = this.args.label && this.args.label.trim();
-    const idIsNotDefined = this.args.id && !this.args.id.trim();
+    const idIsNotDefined = !(this.args.id && this.args.id.trim());
 
     if (labelIsDefined && idIsNotDefined) {
       throw new Error('ERROR in PixTextarea component, @id param is necessary when giving @label');
