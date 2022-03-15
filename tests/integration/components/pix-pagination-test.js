ebd2a5eaf9904ec4c27ed5fe50e1e065b9f88d3d
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-03-15 16:35:12 +0100

    feat: add translations to pix-pagination

--- tests/integration/components/pix-pagination-test.js
+++ tests/integration/components/pix-pagination-test.js
@@ -7,6 +7,12 @@ module('Integration | Component | pagination', function (hooks) {
   setupRenderingTest(hooks);
 
   const COMPONENT_SELECTOR = '.pagination-control';
+  const paginationData = {
+    page: 1,
+    pageSize: 10,
+    rowCount: 2,
+    pageCount: 1,
+  };
 
   test('it renders the default PixPagination', async function (assert) {
     // when
@@ -17,5 +23,39 @@ module('Integration | Component | pagination', function (hooks) {
 
     // then
     assert.equal(componentElement.textContent.trim(), 'content');
+  });
+
+  test('Use locale params to translate component', async function (assert) {
+    // given
+    this.locale = 'en';
+    const COMPONENT_SELECTOR = '.page-size__label';
+    this.set('pagination', paginationData);
+
+    // when
+    await render(hbs`
+      <PixPagination
+        @locale={{this.locale}}
+        @pagination={{pagination}}
+      />
+    `);
+    const componentElement = this.element.querySelector(COMPONENT_SELECTOR);
+
+    // then
+    assert.equal(componentElement.textContent.trim(), 'See');
+  });
+  test('Use PixPagination without locale params', async function (assert) {
+    // given
+    const COMPONENT_SELECTOR = '.page-size__label';
+    this.set('pagination', paginationData);
+    // when
+    await render(hbs`
+      <PixPagination
+         @pagination={{pagination}}
+      />
+    `);
+    const componentElement = this.element.querySelector(COMPONENT_SELECTOR);
+
+    // then
+    assert.equal(componentElement.textContent.trim(), 'Voir');
   });
 });
