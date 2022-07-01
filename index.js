83d9f6ed9ea8fa31a07807464e927cf5d0939d21
Clarisse Damon <clarisse.damon@pix.fr>
2022-07-01 16:57:03 +0200

    fix: indicate that addon should copy files from public folder

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
+let Funnel = require('broccoli-funnel');
+let mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: require('./package').name,
+  treeForPublic: function (tree) {
+    var assetsTree = new Funnel('public');
+    return mergeTrees([tree, assetsTree], {
+      overwrite: true,
+    });
+  },
 };
