82f686def5c2d416c254d0da2be33d027912d20e
Xavier Carron <xavier.carron@zenika.com>
2022-09-06 11:01:20 +0200

    feat(checkbox): authorize component as label

--- addon/components/pix-checkbox.js
+++ addon/components/pix-checkbox.js
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-import { htmlSafe } from '@ember/string';
 
 const labelSizeToClass = new Map([
   ['small', 'pix-checkbox__label--small'],
@@ -11,17 +10,9 @@ export default class PixCheckbox extends Component {
   constructor() {
     super(...arguments);
 
-    if (!this.args.label) {
-      throw new Error('ERROR in PixCheckbox component, you must provide @label params');
-    }
-    this.label = htmlSafe(this.args.label);
-  }
-
-  get id() {
     if (!this.args.id || !this.args.id.toString().trim()) {
       throw new Error('ERROR in PixCheckbox component, @id param is not provided');
     }
-    return this.args.id;
   }
 
   get labelSizeClass() {
