19a57a32fa8644df9c5ed985b293894562ab960b
François de Metz <francois.de-metz@pix.fr>
2023-03-13 15:28:44 +0100

    Move @formatjs/intl to dependencies and remove custom import to use ember-auto-import
    Co-authored-by: Yoan de Luca <yoan.de-luca@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,13 +6,6 @@ module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
   });
-
-  app.import('node_modules/color/index.js', {
-    using: [{ transformation: 'cjs', as: 'color' }],
-  });
-  app.import('node_modules/lodash.debounce/index.js', {
-    using: [{ transformation: 'cjs', as: 'lodash.debounce' }],
-  });
   /*
     This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
