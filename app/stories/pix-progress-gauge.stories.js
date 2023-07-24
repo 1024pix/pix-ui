ac8e74faa6b99ce2d16bec10b98a594fecff7fb1
François de Metz <francois.de-metz@pix.fr>
2023-07-24 18:01:21 +0200

    Migrate PixProgressGauge to mdx2

--- app/stories/pix-progress-gauge.stories.js
+++ app/stories/pix-progress-gauge.stories.js
@@ -1,7 +1,46 @@
 import { hbs } from 'ember-cli-htmlbars';
 
 export default {
-  component: 'PixProgressGauge',
+  title: 'Others/Progress Gauge',
+  argTypes: {
+    value: {
+      name: 'value',
+      description: 'Valeur atteinte sur 100',
+      type: { name: 'number', required: true },
+      table: { defaultValue: { summary: null } },
+    },
+    label: {
+      name: 'label',
+      description:
+        "Afficher un label caché permettant d'expliciter le contexte de la jauge de progression",
+      type: { name: 'string', required: true },
+      table: { defaultValue: { summary: 'null' } },
+    },
+    themeMode: {
+      name: 'themeMode',
+      description:
+        "Permet d'indiquer si le thème de la barre de progression est en dark mode ou light mode. Modifie la couleur de fond de la barre de progression. Peut prendre les valeurs `light` ou `dark`",
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: 'light' } },
+      control: { type: 'select' },
+      options: ['dark', 'light'],
+    },
+    color: {
+      name: 'color',
+      description:
+        "Modifie la couleur du contenu de la barre de progression. Peut prendre les valeurs `blue`, 'green' ou `purple`",
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: 'blue' } },
+      control: { type: 'select' },
+      options: ['blue', 'green', 'purple'],
+    },
+    subtitle: {
+      name: 'subtitle',
+      description: 'Afficher un sous-titre sous la barre de progression',
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: 'null' } },
+    },
+  },
 };
 
 export const Default = (args) => {
@@ -40,44 +79,4 @@ darkModeProgressGauge.args = {
   color: 'purple',
   themeMode: 'dark',
   subtitle: 'Avancement',
-};
-
-export const argTypes = {
-  value: {
-    name: 'value',
-    description: 'Valeur atteinte sur 100',
-    type: { name: 'number', required: true },
-    table: { defaultValue: { summary: null } },
-  },
-  label: {
-    name: 'label',
-    description:
-      "Afficher un label caché permettant d'expliciter le contexte de la jauge de progression",
-    type: { name: 'string', required: true },
-    table: { defaultValue: { summary: 'null' } },
-  },
-  themeMode: {
-    name: 'themeMode',
-    description:
-      "Permet d'indiquer si le thème de la barre de progression est en dark mode ou light mode. Modifie la couleur de fond de la barre de progression. Peut prendre les valeurs `light` ou `dark`",
-    type: { name: 'string', required: false },
-    table: { defaultValue: { summary: 'light' } },
-    control: { type: 'select' },
-    options: ['dark', 'light'],
-  },
-  color: {
-    name: 'color',
-    description:
-      "Modifie la couleur du contenu de la barre de progression. Peut prendre les valeurs `blue`, 'green' ou `purple`",
-    type: { name: 'string', required: false },
-    table: { defaultValue: { summary: 'blue' } },
-    control: { type: 'select' },
-    options: ['blue', 'green', 'purple'],
-  },
-  subtitle: {
-    name: 'subtitle',
-    description: 'Afficher un sous-titre sous la barre de progression',
-    type: { name: 'string', required: false },
-    table: { defaultValue: { summary: 'null' } },
-  },
 };
