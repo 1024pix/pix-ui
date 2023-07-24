9af74fa5f224c0e82dbc4fd627b350c5a4924c27
Mickaël Alibert <mickael.alibert@pix.fr>
2023-07-24 18:11:50 +0200

    :truck: migrate pix-stars mdx2

--- app/stories/pix-stars.stories.js
+++ app/stories/pix-stars.stories.js
@@ -1,7 +1,32 @@
 import { hbs } from 'ember-cli-htmlbars';
 
 export default {
-  component: 'PixStars',
+  title: 'Others/PixStars',
+  argTypes: {
+    count: {
+      name: 'count',
+      description: 'Nombre total d’étoiles actives',
+      type: { name: 'number', required: false },
+    },
+    total: {
+      name: 'total',
+      description: 'Nombre total d’étoiles',
+      type: { name: 'number', required: false },
+    },
+    alt: {
+      name: 'alt',
+      description: 'Message alternatif pour les étoiles',
+      type: { name: 'string', required: true },
+    },
+    color: {
+      name: 'color',
+      description: 'Couleur des étoiles',
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: undefined } },
+      control: { type: 'radio' },
+      options: [undefined, 'blue', 'grey'],
+    },
+  },
 };
 
 const Template = (args) => {
@@ -17,30 +42,4 @@ stars.args = {
   total: 5,
   alt: 'message alternatif',
   color: undefined,
-};
-
-export const argTypes = {
-  count: {
-    name: 'count',
-    description: 'Nombre total d’étoiles actives',
-    type: { name: 'number', required: false },
-  },
-  total: {
-    name: 'total',
-    description: 'Nombre total d’étoiles',
-    type: { name: 'number', required: false },
-  },
-  alt: {
-    name: 'alt',
-    description: 'Message alternatif pour les étoiles',
-    type: { name: 'string', required: true },
-  },
-  color: {
-    name: 'color',
-    description: 'Couleur des étoiles',
-    type: { name: 'string', required: false },
-    table: { defaultValue: { summary: undefined } },
-    control: { type: 'radio' },
-    options: [undefined, 'blue', 'grey'],
-  },
 };
