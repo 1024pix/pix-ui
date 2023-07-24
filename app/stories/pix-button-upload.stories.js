9d4dcce759856057a42555eeac7fff0cc07a4c57
Marianne Bost <marianne.bost@pix.fr>
2023-07-24 16:31:46 +0200

    Migrate pix-button-upload to mdx2

--- app/stories/pix-button-upload.stories.js
+++ app/stories/pix-button-upload.stories.js
@@ -2,7 +2,65 @@ import { hbs } from 'ember-cli-htmlbars';
 import { action } from '@storybook/addon-actions';
 
 export default {
-  component: 'PixButtonUpload',
+  title: 'Basics/ButtonUpload',
+  argTypes: {
+    id: {
+      name: 'id',
+      description: "identifiant du bouton d'upload",
+      type: { name: 'string', required: true },
+    },
+    onChange: {
+      name: 'onChange',
+      description:
+        "fonction à exécuter au moment de l'upload du fichier, elle prend en entrée la liste des fichiers uploadés.",
+      type: { name: 'function', required: true },
+    },
+    shape: {
+      name: 'shape',
+      description: 'forme: `rounded`,`squircle`',
+      type: { name: 'string', required: false },
+      options: ['rounded', 'squircle'],
+      control: { type: 'select' },
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'squircle' },
+      },
+    },
+    backgroundColor: {
+      name: 'backgroundColor',
+      description:
+        'color: `blue`, `green`, `yellow`, `red`, `grey`, `transparent-light`, `transparent-dark`',
+      options: ['blue', 'green', 'yellow', 'red', 'grey', 'transparent-light', 'transparent-dark'],
+      type: { name: 'string', required: false },
+      control: { type: 'select' },
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'blue' },
+      },
+    },
+    size: {
+      name: 'size',
+      description: 'taille: `big`,`small`',
+      options: ['big', 'small'],
+      type: { name: 'string', required: false },
+      control: { type: 'select' },
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'big' },
+      },
+    },
+    isBorderVisible: {
+      name: 'isBorderVisible',
+      description:
+        'Paramètre utilisé seulement quand le `backgroundColor` est `transparent-light` ou `transparent-dark`',
+      type: { name: 'boolean', required: false },
+      control: { type: 'boolean' },
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+  },
 };
 
 export const buttonUpload = (args) => {
@@ -24,63 +82,4 @@ export const buttonUpload = (args) => {
 buttonUpload.args = {
   id: 'file-upload',
   onChange: action('onChange'),
-};
-
-export const argTypes = {
-  id: {
-    name: 'id',
-    description: "identifiant du bouton d'upload",
-    type: { name: 'string', required: true },
-  },
-  onChange: {
-    name: 'onChange',
-    description:
-      "fonction à exécuter au moment de l'upload du fichier, elle prend en entrée la liste des fichiers uploadés.",
-    type: { name: 'function', required: true },
-  },
-  shape: {
-    name: 'shape',
-    description: 'forme: `rounded`,`squircle`',
-    type: { name: 'string', required: false },
-    options: ['rounded', 'squircle'],
-    control: { type: 'select' },
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: 'squircle' },
-    },
-  },
-  backgroundColor: {
-    name: 'backgroundColor',
-    description:
-      'color: `blue`, `green`, `yellow`, `red`, `grey`, `transparent-light`, `transparent-dark`',
-    options: ['blue', 'green', 'yellow', 'red', 'grey', 'transparent-light', 'transparent-dark'],
-    type: { name: 'string', required: false },
-    control: { type: 'select' },
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: 'blue' },
-    },
-  },
-  size: {
-    name: 'size',
-    description: 'taille: `big`,`small`',
-    options: ['big', 'small'],
-    type: { name: 'string', required: false },
-    control: { type: 'select' },
-    table: {
-      type: { summary: 'string' },
-      defaultValue: { summary: 'big' },
-    },
-  },
-  isBorderVisible: {
-    name: 'isBorderVisible',
-    description:
-      'Paramètre utilisé seulement quand le `backgroundColor` est `transparent-light` ou `transparent-dark`',
-    type: { name: 'boolean', required: false },
-    control: { type: 'boolean' },
-    table: {
-      type: { summary: 'boolean' },
-      defaultValue: { summary: 'false' },
-    },
-  },
 };
