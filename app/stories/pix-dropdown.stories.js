386e16a9039269975b2af7b7dd1beadf890c469f
Marianne Bost <marianne.bost@pix.fr>
2023-07-24 17:55:48 +0200

    Migrate pix-dropdown to mdx2

--- app/stories/pix-dropdown.stories.js
+++ app/stories/pix-dropdown.stories.js
@@ -2,10 +2,119 @@ import { hbs } from 'ember-cli-htmlbars';
 import { action } from '@storybook/addon-actions';
 
 export default {
-  component: 'PixDropdown',
+  title: 'Form/Dropdown',
+  argTypes: {
+    options: {
+      name: 'options',
+      description:
+        'Les options sont représentées par un tableau d‘objet contenant les propriétés ``value`` et ``label``.',
+      type: { name: 'array', required: true },
+    },
+    selectedOption: {
+      name: 'selectedOption',
+      description: 'Option sélectionnée',
+      options: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+      control: { type: 'select' },
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    onSelect: {
+      name: 'onSelect',
+      description: 'Fonction à appeler quand une option est sélectionnée.',
+      type: { required: true },
+      control: { disable: true },
+    },
+    isSearchable: {
+      name: 'isSearchable',
+      description:
+        "Rend le champ cherchable, le paramètre @searchPlaceholder devient requis pour l'accessibilité.",
+      control: { type: 'boolean' },
+      type: { name: 'boolean', required: false },
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: false },
+      },
+    },
+    searchPlaceholder: {
+      name: 'searchPlaceholder',
+      description: 'Placeholder du champ de recherche. Requis si le menu est cherchable.',
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    placeholder: {
+      name: 'placeholder',
+      description: "Un texte donnant une indication a l'utilisateur sur le choix des options.",
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    label: {
+      name: 'label',
+      description:
+        'Label du menu déroulant, le paramètre @id devient obligatoire avec ce paramètre.',
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    requiredLabel: {
+      name: 'requiredLabel',
+      description:
+        'Label indiquant que le champ est requis, le paramètre @label devient obligatoire avec ce paramètre.',
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    id: {
+      name: 'id',
+      description: "L'id du label",
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    pageSize: {
+      name: 'pageSize',
+      description: "Le nombre d'élément à afficher dans la liste.",
+      type: { name: 'number', required: false },
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    clearLabel: {
+      name: 'clearLabel',
+      description: "Label de l'icône pour éliminer la sélection. Requis pour l'accessibilité.",
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    expandLabel: {
+      name: 'expandLabel',
+      description: "Label de l'icône pour ouvrir le menu déroulant. Requis pour l'accessibilité.",
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    collapseLabel: {
+      name: 'collapseLabel',
+      description: "Label de l'icône pour réduire le menu déroulant. Requis pour l'accessibilité.",
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+  },
 };
 
-export const Template = (args) => {
+const Template = (args) => {
   return {
     template: hbs`<div style='width: 300px;'>
   <PixDropdown
@@ -81,113 +190,4 @@ paginatedDropdown.args = {
     value: `${index}`,
     label: `${index}abc`,
   })),
-};
-
-export const argTypes = {
-  options: {
-    name: 'options',
-    description:
-      'Les options sont représentées par un tableau d‘objet contenant les propriétés ``value`` et ``label``.',
-    type: { name: 'array', required: true },
-  },
-  selectedOption: {
-    name: 'selectedOption',
-    description: 'Option sélectionnée',
-    options: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-    control: { type: 'select' },
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  onSelect: {
-    name: 'onSelect',
-    description: 'Fonction à appeler quand une option est sélectionnée.',
-    type: { required: true },
-    control: { disable: true },
-  },
-  isSearchable: {
-    name: 'isSearchable',
-    description:
-      "Rend le champ cherchable, le paramètre @searchPlaceholder devient requis pour l'accessibilité.",
-    control: { type: 'boolean' },
-    type: { name: 'boolean', required: false },
-    table: {
-      type: { summary: 'boolean' },
-      defaultValue: { summary: false },
-    },
-  },
-  searchPlaceholder: {
-    name: 'searchPlaceholder',
-    description: 'Placeholder du champ de recherche. Requis si le menu est cherchable.',
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  placeholder: {
-    name: 'placeholder',
-    description: "Un texte donnant une indication a l'utilisateur sur le choix des options.",
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  label: {
-    name: 'label',
-    description: 'Label du menu déroulant, le paramètre @id devient obligatoire avec ce paramètre.',
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  requiredLabel: {
-    name: 'requiredLabel',
-    description:
-      'Label indiquant que le champ est requis, le paramètre @label devient obligatoire avec ce paramètre.',
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  id: {
-    name: 'id',
-    description: "L'id du label",
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  pageSize: {
-    name: 'pageSize',
-    description: "Le nombre d'élément à afficher dans la liste.",
-    type: { name: 'number', required: false },
-    table: {
-      type: { summary: 'number' },
-    },
-  },
-  clearLabel: {
-    name: 'clearLabel',
-    description: "Label de l'icône pour éliminer la sélection. Requis pour l'accessibilité.",
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  expandLabel: {
-    name: 'expandLabel',
-    description: "Label de l'icône pour ouvrir le menu déroulant. Requis pour l'accessibilité.",
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
-  collapseLabel: {
-    name: 'collapseLabel',
-    description: "Label de l'icône pour réduire le menu déroulant. Requis pour l'accessibilité.",
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
 };
