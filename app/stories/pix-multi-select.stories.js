552018355ce8d9c71d1e31b2a3064acf9c5cb8d2
François de Metz <francois.de-metz@pix.fr>
2023-07-24 18:11:40 +0200

    Migrate PixMultiSelect to mdx2

--- app/stories/pix-multi-select.stories.js
+++ app/stories/pix-multi-select.stories.js
@@ -2,11 +2,9 @@ import { hbs } from 'ember-cli-htmlbars';
 import { action } from '@storybook/addon-actions';
 
 export default {
-  component: 'PixMultiSelect',
-};
-
-const Template = (args) => ({
-  template: hbs`
+  title: 'Form/Multi Select',
+  render: (args) => ({
+    template: hbs`
   <style>
     .custom {
       border : none;
@@ -32,8 +30,79 @@ const Template = (args) => ({
     @options={{this.options}} as |option|
   >{{option.label}}</PixMultiSelect>
  `,
-  context: args,
-});
+    context: args,
+  }),
+  argTypes: {
+    id: {
+      name: 'id',
+      description:
+        "Permet l'accessibilité du composant attribuant des ``for`` pour chaque entité. **⚠️ L'`id` est obligatoire que si le `label` n'est pas donné. ⚠️**",
+      type: { name: 'string' },
+    },
+    placeholder: {
+      name: 'placeholder',
+      description:
+        'Rempli le contenu interne du composant, `placeholder` pour `isSearchable` `true`, sinon rawContent du `button`',
+      type: { name: 'string', required: true },
+    },
+    label: {
+      name: 'label',
+      description:
+        "Donne un label au champ qui sera celui vocalisé par le lecteur d'écran. **⚠️ Le`label` est obligatoire que si l'`id` n'est pas donné. ⚠️**",
+      type: { name: 'string' },
+    },
+    screenReaderOnly: {
+      name: 'screenReaderOnly',
+      description: "Permet de cacher à l'écran le label tout en restant vocalisable",
+      type: { name: 'boolean', required: false },
+      table: { defaultValue: { summary: false } },
+    },
+    emptyMessage: {
+      name: 'emptyMessage',
+      description:
+        'Un intitulé de choix indisponible (dans le cas ou certains filtres seraient excluant)',
+      type: { name: 'string', required: true },
+    },
+    options: {
+      name: 'options',
+      description:
+        'Les options sont représentées par un tableau d‘objet contenant les propriétés ``value`` et ``label``. ``value`` doit être de type ``String`` pour être conforme au traitement des input value.',
+      type: { name: 'array', required: false },
+    },
+    onChange: {
+      name: 'onChange',
+      description: "Une fonction permettant d'effectuer une action à chaque sélection",
+      type: { required: true },
+    },
+    values: {
+      name: 'values',
+      description: 'Une pré-sélection peut être donnée au composant',
+      type: { name: 'array', required: false },
+    },
+    isSearchable: {
+      name: 'isSearchable',
+      description: 'Permet de rajouter une saisie utilisateur pour faciliter la recherche',
+      type: { name: 'boolean', required: false },
+      table: { defaultValue: { summary: false } },
+    },
+    strictSearch: {
+      name: 'strictSearch',
+      description:
+        'Permet de rendre sensible à la casse et au diacritiques lorsque ``isSearchable`` à ``true``',
+      type: { name: 'boolean', required: false },
+      table: { defaultValue: { summary: false } },
+    },
+    className: {
+      name: 'className',
+      description: 'Cette classe css permet de surcharger le css par défaut du composant.',
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+  },
+};
+
 const DEFAULT_OPTIONS = [
   { label: 'ANETH HERBE AROMATIQUE', value: '1' },
   { label: 'ANIS VERT HERBE AROMATIQUE', value: '2' },
@@ -48,14 +117,15 @@ const DEFAULT_OPTIONS = [
   { label: 'CERFEUIL HERBE AROMATIQUE', value: '11' },
 ];
 
-export const Default = Template.bind({});
-Default.args = {
-  label: 'Label du champ',
-  options: DEFAULT_OPTIONS,
-  onChange: action('onChange'),
-  emptyMessage: 'pas de résultat',
-  values: ['1', '3'],
-  placeholder: 'placeholder',
+export const Default = {
+  args: {
+    label: 'Label du champ',
+    options: DEFAULT_OPTIONS,
+    onChange: action('onChange'),
+    emptyMessage: 'pas de résultat',
+    values: ['1', '3'],
+    placeholder: 'placeholder',
+  },
 };
 
 export const multiSelectWithChildComponent = (args) => {
@@ -94,27 +164,30 @@ multiSelectWithChildComponent.args = {
   ],
 };
 
-export const multiSelectSearchable = Template.bind({});
-multiSelectSearchable.args = {
-  ...Default.args,
-  isSearchable: true,
-  strictSearch: true,
-  emptyMessage: 'Aucune option trouvée',
+export const multiSelectSearchable = {
+  args: {
+    ...Default.args,
+    isSearchable: true,
+    strictSearch: true,
+    emptyMessage: 'Aucune option trouvée',
+  },
 };
 
-export const multiSelectWithCustomClass = Template.bind({});
-multiSelectWithCustomClass.args = {
-  ...Default.args,
-  className: 'custom',
-  isSearchable: false,
+export const multiSelectWithCustomClass = {
+  args: {
+    ...Default.args,
+    className: 'custom',
+    isSearchable: false,
+  },
 };
 
-export const multiSelectWithId = Template.bind({});
-multiSelectWithId.args = {
-  ...Default.args,
-  label: undefined,
-  id: 'custom',
-  isSearchable: false,
+export const multiSelectWithId = {
+  args: {
+    ...Default.args,
+    label: undefined,
+    id: 'custom',
+    isSearchable: false,
+  },
 };
 
 const TemplateWithYield = (args) => ({
@@ -143,74 +216,4 @@ multiSelectWithYield.args = {
   ...Default.args,
   placeholder: undefined,
   isSearchable: false,
-};
-
-export const argTypes = {
-  id: {
-    name: 'id',
-    description:
-      "Permet l'accessibilité du composant attribuant des ``for`` pour chaque entité. **⚠️ L'`id` est obligatoire que si le `label` n'est pas donné. ⚠️**",
-    type: { name: 'string' },
-  },
-  placeholder: {
-    name: 'placeholder',
-    description:
-      'Rempli le contenu interne du composant, `placeholder` pour `isSearchable` `true`, sinon rawContent du `button`',
-    type: { name: 'string', required: true },
-  },
-  label: {
-    name: 'label',
-    description:
-      "Donne un label au champ qui sera celui vocalisé par le lecteur d'écran. **⚠️ Le`label` est obligatoire que si l'`id` n'est pas donné. ⚠️**",
-    type: { name: 'string' },
-  },
-  screenReaderOnly: {
-    name: 'screenReaderOnly',
-    description: "Permet de cacher à l'écran le label tout en restant vocalisable",
-    type: { name: 'boolean', required: false },
-    table: { defaultValue: { summary: false } },
-  },
-  emptyMessage: {
-    name: 'emptyMessage',
-    description:
-      'Un intitulé de choix indisponible (dans le cas ou certains filtres seraient excluant)',
-    type: { name: 'string', required: true },
-  },
-  options: {
-    name: 'options',
-    description:
-      'Les options sont représentées par un tableau d‘objet contenant les propriétés ``value`` et ``label``. ``value`` doit être de type ``String`` pour être conforme au traitement des input value.',
-    type: { name: 'array', required: false },
-  },
-  onChange: {
-    name: 'onChange',
-    description: "Une fonction permettant d'effectuer une action à chaque sélection",
-    type: { required: true },
-  },
-  values: {
-    name: 'values',
-    description: 'Une pré-sélection peut être donnée au composant',
-    type: { name: 'array', required: false },
-  },
-  isSearchable: {
-    name: 'isSearchable',
-    description: 'Permet de rajouter une saisie utilisateur pour faciliter la recherche',
-    type: { name: 'boolean', required: false },
-    table: { defaultValue: { summary: false } },
-  },
-  strictSearch: {
-    name: 'strictSearch',
-    description:
-      'Permet de rendre sensible à la casse et au diacritiques lorsque ``isSearchable`` à ``true``',
-    type: { name: 'boolean', required: false },
-    table: { defaultValue: { summary: false } },
-  },
-  className: {
-    name: 'className',
-    description: 'Cette classe css permet de surcharger le css par défaut du composant.',
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
 };
