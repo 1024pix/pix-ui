a555d2b435cccb42171aa8acb9d2fd919a46b2d8
François de Metz <francois.de-metz@pix.fr>
2023-07-24 16:46:16 +0200

    Migrate PixMessage to mdx2

--- app/stories/pix-message.stories.js
+++ app/stories/pix-message.stories.js
@@ -1,11 +1,8 @@
 import { hbs } from 'ember-cli-htmlbars';
 
 export default {
-  component: 'PixMessage',
-};
-
-const Template = (args) => {
-  return {
+  title: 'Notification/Message',
+  render: (args) => ({
     template: hbs`<PixMessage @type={{this.type}} @withIcon={{this.withIcon}}>
   Ceci est un message
   {{this.type}}
@@ -14,48 +11,51 @@ const Template = (args) => {
   de l'afficher sur deux lignes.
 </PixMessage>`,
     context: args,
-  };
-};
-
-export const Default = Template.bind({});
-
-export const error = Template.bind({});
-error.args = {
-  type: 'error',
-  withIcon: true,
-};
-
-export const warning = Template.bind({});
-warning.args = {
-  type: 'warning',
-  withIcon: true,
-};
-
-export const success = Template.bind({});
-success.args = {
-  type: 'success',
-  withIcon: true,
-};
-
-export const withIcon = Template.bind({});
-withIcon.args = {
-  withIcon: true,
-};
-
-export const argTypes = {
-  type: {
-    name: 'type',
-    description: 'Type du message',
-    type: { name: 'string', required: false },
-    table: { defaultValue: { summary: 'info' } },
-    control: { type: 'select' },
-    options: ['info', 'success', 'warning', 'alert', 'error'],
+  }),
+  argTypes: {
+    type: {
+      name: 'type',
+      description: 'Type du message',
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: 'info' } },
+      control: { type: 'select' },
+      options: ['info', 'success', 'warning', 'alert', 'error'],
+    },
+    withIcon: {
+      name: 'withIcon',
+      description: 'Icône du message',
+      table: { defaultValue: { summary: false } },
+      type: { name: 'boolean', required: false },
+      control: { type: 'boolean' },
+    },
   },
-  withIcon: {
-    name: 'withIcon',
-    description: 'Icône du message',
-    table: { defaultValue: { summary: false } },
-    type: { name: 'boolean', required: false },
-    control: { type: 'boolean' },
+};
+
+export const Default = {}
+
+export const error = {
+  args: {
+    type: 'error',
+    withIcon: true,
+  }
+};
+
+export const warning = {
+  args: {
+    type: 'warning',
+    withIcon: true,
+  }
+};
+
+export const success = {
+  args: {
+    type: 'success',
+    withIcon: true,
   },
+};
+
+export const withIcon = {
+  args: {
+    withIcon: true,
+  }
 };
