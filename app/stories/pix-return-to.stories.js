d6430b277dd3bb8bb82a780a28128bffe0f6bf9f
François de Metz <francois.de-metz@pix.fr>
2023-07-24 17:49:18 +0200

    Migrate PixReturnTo to mdx2

--- app/stories/pix-return-to.stories.js
+++ app/stories/pix-return-to.stories.js
@@ -1,45 +1,41 @@
 import { hbs } from 'ember-cli-htmlbars';
 
 export default {
-  component: 'PixReturnTo',
-};
-
-export const returnTo = (args) => {
-  return {
-    template: hbs`<PixReturnTo @route='profile' @shade={{this.shade}} />`,
+  title: 'Basics/Return To',
+  render: (args) => ({
+    template: hbs`<PixReturnTo @route='profile' @shade={{this.shade}}>{{this.content}}</PixReturnTo>`,
     context: args,
-  };
-};
-returnTo.args = {
-  shade: 'blue',
+  }),
+  argTypes: {
+    route: {
+      name: 'route',
+      description: 'Route de redirection',
+      type: { name: 'string', required: true },
+    },
+    model: {
+      name: 'model',
+      description: 'Model Ember',
+      type: { required: false },
+    },
+    shade: {
+      name: 'shade',
+      description: 'Couleur du lien',
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: 'black' } },
+      control: { type: 'select' },
+      options: ['white', 'black', 'blue'],
+    },
+  },
 };
 
-export const returnToWithText = (args) => {
-  return {
-    template: hbs`<PixReturnTo @route='profile' @shade={{this.shade}}>
-  Retour vers mon profil
-</PixReturnTo>`,
-    context: args,
-  };
+export const returnTo = {
+  args: {
+    shade: 'blue',
+  },
 };
 
-export const argTypes = {
-  route: {
-    name: 'route',
-    description: 'Route de redirection',
-    type: { name: 'string', required: true },
-  },
-  model: {
-    name: 'model',
-    description: 'Model Ember',
-    type: { required: false },
-  },
-  shade: {
-    name: 'shade',
-    description: 'Couleur du lien',
-    type: { name: 'string', required: false },
-    table: { defaultValue: { summary: 'black' } },
-    control: { type: 'select' },
-    options: ['white', 'black', 'blue'],
+export const returnToWithText = {
+  args: {
+    content: 'Retour vers mon profil',
   },
 };
