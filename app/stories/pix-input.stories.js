8897a4dbc66c1d2c170cf7909ea1f7f5c1be216e
Marianne Bost <marianne.bost@pix.fr>
2023-07-25 10:15:15 +0200

    Migrate pix-input to mdx2

--- app/stories/pix-input.stories.js
+++ app/stories/pix-input.stories.js
@@ -1,10 +1,60 @@
 import { hbs } from 'ember-cli-htmlbars';
 
 export default {
-  component: 'PixInput',
+  title: 'Form/Inputs/Input',
+  argTypes: {
+    ariaLabel: {
+      name: 'ariaLabel',
+      description: "L'action du champ, pour l'accessibilité. Requis si label n'est pas définit.",
+      type: { name: 'string', required: false },
+    },
+    id: {
+      name: 'id',
+      description: 'Identifiant du champ permettant de lui attacher un label',
+      type: { name: 'string', required: true },
+    },
+    value: {
+      name: 'value',
+      description: "Valeur de l'input",
+      type: { name: 'string', required: false },
+    },
+    label: {
+      name: 'label',
+      description: "Le label de l'input",
+      type: { name: 'string', required: false },
+    },
+    information: {
+      name: 'information',
+      description: 'Un descriptif complétant le label',
+      type: { name: 'string', required: false },
+    },
+    validationStatus: {
+      name: 'validationStatus',
+      description:
+        "Définit l'état du champ, neutre par défaut, en succès ou erreur selon l'action de l'utilisateur",
+      type: { name: 'string', required: false },
+      options: ['default', 'success', 'error'],
+      control: { type: 'select' },
+    },
+    errorMessage: {
+      name: 'errorMessage',
+      description:
+        "Affiche le message d'erreur donné. Doit s'accompagner du paramètre validationStatus en 'error'",
+      type: { name: 'string', required: false },
+    },
+    requiredLabel: {
+      name: 'requiredLabel',
+      description:
+        'Label indiquant que le champ est requis, le paramètre @label devient obligatoire avec ce paramètre.',
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+  },
 };
 
-export const Template = (args) => {
+const Template = (args) => {
   return {
     template: hbs`<PixInput
   @id={{this.id}}
@@ -53,55 +103,4 @@ withRequiredLabel.args = {
   id: 'first-name',
   label: 'Prénom',
   requiredLabel: 'Champ obligatoire',
-};
-
-export const argTypes = {
-  ariaLabel: {
-    name: 'ariaLabel',
-    description: "L'action du champ, pour l'accessibilité. Requis si label n'est pas définit.",
-    type: { name: 'string', required: false },
-  },
-  id: {
-    name: 'id',
-    description: 'Identifiant du champ permettant de lui attacher un label',
-    type: { name: 'string', required: true },
-  },
-  value: {
-    name: 'value',
-    description: "Valeur de l'input",
-    type: { name: 'string', required: false },
-  },
-  label: {
-    name: 'label',
-    description: "Le label de l'input",
-    type: { name: 'string', required: false },
-  },
-  information: {
-    name: 'information',
-    description: 'Un descriptif complétant le label',
-    type: { name: 'string', required: false },
-  },
-  validationStatus: {
-    name: 'validationStatus',
-    description:
-      "Définit l'état du champ, neutre par défaut, en succès ou erreur selon l'action de l'utilisateur",
-    type: { name: 'string', required: false },
-    options: ['default', 'success', 'error'],
-    control: { type: 'select' },
-  },
-  errorMessage: {
-    name: 'errorMessage',
-    description:
-      "Affiche le message d'erreur donné. Doit s'accompagner du paramètre validationStatus en 'error'",
-    type: { name: 'string', required: false },
-  },
-  requiredLabel: {
-    name: 'requiredLabel',
-    description:
-      'Label indiquant que le champ est requis, le paramètre @label devient obligatoire avec ce paramètre.',
-    type: { name: 'string', required: false },
-    table: {
-      type: { summary: 'string' },
-    },
-  },
 };
