0f382a627457427c57813b728fc5c7fa82e3174f
Andreia Pena <andreia.pena@pix.fr>
2023-11-07 14:16:18 +0100

    (wip) create pix-toast service

--- addon/components/pix-toast.js
+++ addon/components/pix-toast.js
@@ -1,15 +1,21 @@
 import Component from '@glimmer/component';
-
+import { service } from '@ember/service';
+import { action } from '@ember/object';
 const TYPE_SUCCESS = 'success';
 const TYPE_ERROR = 'error';
 const TYPE_INFORMATION = 'information';
 const TYPE_WARNING = 'warning';
 
 export default class PixToast extends Component {
+  @service toast;
+
   get type() {
-    const error = console.warn('ERROR in PixToast component, you need to provide a type.');
     const correctTypes = [TYPE_SUCCESS, TYPE_ERROR, TYPE_INFORMATION, TYPE_WARNING];
-    return correctTypes.includes(this.args.type) ? this.args.type : error;
+    const isACorrectType = correctTypes.includes(this.args.type);
+    if (!isACorrectType) {
+      throw new Error('ERROR in PixToast component, you need to provide a type.');
+    }
+    return this.args.type;
   }
 
   get iconClass() {
@@ -20,5 +26,12 @@ export default class PixToast extends Component {
       [TYPE_WARNING]: 'triangle-exclamation',
     };
     return classes[this.type];
+  }
+
+  @action
+  removeNotification(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.toast.removeNotification(this.toast);
   }
 }
