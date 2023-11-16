d1d0f1d86926cfde92423c734d06954eb4616775
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-11-16 15:45:59 +0100

    feat: use new color variables

--- scripts/migrate-spacing-scss-vars-to-css-vars.sh
+++ scripts/migrate-spacing-scss-vars-to-css-vars.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-for f in $(find . -name '*.scss' -not -path './node_modules/*' -not -path './addon/styles/pix-design-tokens/_spacing.scss')
+for f in $(find . -name '*.scss' -not -path './node_modules/*' -not -name '_spacing.scss')
 do
+  echo "parsing file" $f
   sed -i '' -e 's/$pix-spacing-xxs/var(--pix-spacing-1x)/g' "$f"
   sed -i '' -e 's/$pix-spacing-xs/var(--pix-spacing-2x)/g' "$f"
   sed -i '' -e 's/$pix-spacing-s/var(--pix-spacing-4x)/g' "$f"
