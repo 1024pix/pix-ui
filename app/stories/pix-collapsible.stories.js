e2d9a3182a16df60913d44dc795a491fee07c485
Andreia Pena <andreia.pena@pix.fr>
2024-01-18 17:58:48 +0100

    ✨ pix-ui: add tag on Pix Collapsible

--- app/stories/pix-collapsible.stories.js
+++ app/stories/pix-collapsible.stories.js
@@ -13,6 +13,18 @@ export default {
       description: "Ajoute l'icône donnée en paramètre avant le titre du PixCollapsible",
       type: { name: 'string', required: false },
     },
+    tagContent: {
+      name: 'tagContent',
+      description: "Contenu du tag qui s'affiche près du chevron",
+      type: { name: 'string', required: false },
+    },
+    tagColor: {
+      name: 'tagColor',
+      description:
+        "Couleur du tag qui s'affiche près du chevron. Doit s'ajouter avec le paramètre tagContent. Voir le composant Pix Tag pour les couleurs disponibles",
+      type: { name: 'string', required: false },
+      table: { defaultValue: { summary: 'primary' } },
+    },
   },
 };
 
@@ -58,6 +70,33 @@ export const multipleCollapsible = (args) => {
 
   <PixCollapsible @title='Titre C' @titleIcon={{this.titleIcon}}>
     <div>Contenu C</div>
+  </PixCollapsible>
+</div>`,
+    context: args,
+  };
+};
+
+export const collapsibleWithTag = (args) => {
+  return {
+    template: hbs`{{! template-lint-disable no-inline-styles }}
+<div style='width:800px'>
+  {{! template-lint-disable no-inline-styles }}
+  <PixCollapsible
+    @title='Titre A'
+    @titleIcon={{this.titleIcon}}
+    @tagColor='success'
+    @tagContent='tag 1'
+  >
+    <div>Contenu A</div>
+  </PixCollapsible>
+
+  <PixCollapsible
+    @title='Titre B'
+    @titleIcon={{this.titleIcon}}
+    @tagColor='error'
+    @tagContent='tag 2'
+  >
+    <div>Contenu B</div>
   </PixCollapsible>
 </div>`,
     context: args,
