301df4ff3f30339d704fbac61337a5558597d16c
mcampourcy <mireille.campourcy@pix.fr>
2024-02-13 15:05:10 +0100

    :sparkles: Add Label Component

---
+++ src/stories/Label/Label.vue
@@ -0,0 +1,29 @@
+<template>
+  <label>
+    <abbr title={} class="mandatory-mark" v-if={requiredLabel}>*</abbr>
+
+    {{label}}
+
+    <span class="pix-label__sub-label" v-if={subLabel}>{{subLabel}}</span>
+  </label>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import './label.css';
+
+const label = ref({
+  type: String,
+  required: true,
+})
+
+const requiredLabel = ref({
+  default: false,
+  type: String,
+  required: true,
+})
+
+const subLabel = ref({
+  type: String,
+})
+</script>
