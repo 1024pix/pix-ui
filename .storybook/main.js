c2cd7efa62bda533afcc1e092d54457ebba4e5bf
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-03-14 21:35:52 +0100

    feat(storybook): run storybook 8 auto upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,21 @@
 const config = {
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-webpack5-compiler-babel',
+  ],
+
   stories: ['../docs/**/*.@(mdx|stories.@(mdx))', '../app/*/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   staticDirs: ['../dist'],
+
   framework: {
     name: '@1024pix/storybook-ember',
     options: {},
+  },
+
+  docs: {
+    autodocs: true,
   },
 };
 export default config;
