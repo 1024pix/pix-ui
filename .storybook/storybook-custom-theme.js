4362ee6bdc5c81ba72e325a2b78a157c2921ebac
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-05-28 22:26:42 +0200

    fix(storybook): avoid color contrast issues

--- .storybook/storybook-custom-theme.js
+++ .storybook/storybook-custom-theme.js
@@ -3,30 +3,7 @@ import { create } from '@storybook/theming/create';
 import logo from './logo.svg';
 
 export default create({
-  base: 'light', // needed, this is not an optional parameter
-
-  colorPrimary: '#388AFF',
-  colorSecondary: '#388AFF',
-
-  // UI
-  appBg: '#F4F5F7', // background of the global app
-  appContentBg: 'white', // background of the component preview canvas
-  appBorderColor: 'white',
-  appBorderRadius: 4,
-
-  // Text colors of the left menu
-  textColor: 'black',
-  textInverseColor: 'black',
-
-  // Toolbar default and active colors
-  barTextColor: 'white',
-  barSelectedColor: 'white', // Selected tab when multiple tabs
-
-  // Form colors
-  // inputBg: 'white',
-  // inputBorder: 'white',
-  // inputTextColor: 'white',
-  // inputBorderRadius: 4,
+  base: 'light',
   brandTitle: 'Pix',
   brandImage: logo,
 });
