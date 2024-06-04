02d9b531a36eef30caaa3594806e5b67115f22ce
Rébecca <rebecca.kaci@gmail.com>
2024-06-04 16:08:00 +0200

    feat(checkbox): support a variant tile parameter

--- app/stories/pix-checkbox.stories.js
+++ app/stories/pix-checkbox.stories.js
@@ -120,6 +120,24 @@ const FullWidthTemplate = (args) => {
   };
 };
 
+const VariantTileTemplate = (args) => {
+  return {
+    template: hbs`{{! template-lint-disable no-inline-styles }}
+<div
+  style='border: 1px solid var(--pix-neutral-500); padding: var(--pix-spacing-4x); width: 500px'
+><PixCheckbox
+    @id={{this.id}}
+    @isIndeterminate={{this.isIndeterminate}}
+    @checked={{this.checked}}
+    @isDisabled={{this.isDisabled}}
+    @variant='tile'
+  >
+    <:label>{{this.label}}</:label>
+  </PixCheckbox></div>`,
+    context: args,
+  };
+};
+
 export const Default = Template.bind({});
 Default.args = {
   id: 'accept-newsletter',
@@ -135,6 +153,12 @@ DefaultChecked.args = {
 
 export const FullWidth = FullWidthTemplate.bind({});
 FullWidth.args = {
+  id: 'proposal',
+  label: 'Une réponse',
+};
+
+export const VariantTile = VariantTileTemplate.bind({});
+VariantTile.args = {
   id: 'proposal',
   label: 'Une réponse',
 };
@@ -178,6 +202,30 @@ checkedIsDisabled.args = {
 
 export const isIndeterminateIsDisabled = Template.bind({});
 isIndeterminateIsDisabled.args = {
+  id: 'accept-newsletter-2',
+  label: 'Recevoir la newsletter',
+  isDisabled: true,
+  checked: true,
+  isIndeterminate: true,
+};
+
+export const isDisabledVariantTile = VariantTileTemplate.bind({});
+isDisabledVariantTile.args = {
+  id: 'accept-newsletter-2',
+  label: 'Recevoir la newsletter',
+  isDisabled: true,
+};
+
+export const checkedIsDisabledVariantTile = VariantTileTemplate.bind({});
+checkedIsDisabledVariantTile.args = {
+  id: 'accept-newsletter-2',
+  label: 'Recevoir la newsletter',
+  isDisabled: true,
+  checked: true,
+};
+
+export const isIndeterminateIsDisabledVariantTile = VariantTileTemplate.bind({});
+isIndeterminateIsDisabledVariantTile.args = {
   id: 'accept-newsletter-2',
   label: 'Recevoir la newsletter',
   isDisabled: true,
