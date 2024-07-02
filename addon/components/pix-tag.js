5f0900b5b1d59f18bdb9a4fcf3fd731799411cce
Marianne Bost <marianne.bost@pix.fr>
2024-07-02 11:51:31 +0200

    refactor(pix-tag): remove compact option

--- addon/components/pix-tag.js
+++ addon/components/pix-tag.js
@@ -2,10 +2,9 @@ import Component from '@glimmer/component';
 
 export default class PixTag extends Component {
   get classes() {
-    const { color, compact } = this.args;
+    const { color } = this.args;
     const classes = [];
     if (color) classes.push(`pix-tag--${color}`);
-    if (compact) classes.push(`pix-tag--compact`);
     return classes.join(' ');
   }
 }
