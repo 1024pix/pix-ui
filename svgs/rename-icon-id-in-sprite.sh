ed1f1d211e015d821561023b8536ee892e67546c
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-09-09 12:40:59 +0200

    feat(chore): generate sprite from svg folder

---
+++ svgs/rename-icon-id-in-sprite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+for f in $(find . -name 'pix-sprite.svg' -not -path './dist/*')
+do
+  echo "parsing file" $f
+  sed -i '' -e 's/_24dp_E8EAED_FILL0_wght500_GRAD0_opsz24//g' "$f"
+  sed -i '' -e 's/_24dp_E8EAED_FILL1_wght500_GRAD0_opsz24/_plainIcon/g' "$f"
+  sed -i '' -e 's/ fill="#e8eaed"//g' "$f"
+done
---
+++ svgs/svgo.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  multipass: true, // Enable multipass to optimize multiple times
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeViewBox: false,
+          cleanupIds: false,
+        },
+      },
+    },
+  ],
+};
