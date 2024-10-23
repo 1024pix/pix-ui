04468b477ed19c2e7aea737ba8f66049cc765f98
Eric Lim <eric.lim@zenika.com>
2024-10-23 17:34:20 +0200

    feat(pix-ui): remove use of ember array in toast service
    and add jsdoc

--- addon/services/pix-toast.js
+++ addon/services/pix-toast.js
@@ -1,15 +1,26 @@
-import Service from '@ember/service';
 import EmberObject from '@ember/object';
+import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { A } from '@ember/array';
+import { warn } from '@ember/debug';
 
 export default class ToastService extends Service {
-  @tracked content = A();
+  @tracked content = [];
 
+  /**
+   * Creates and returns a new toast notification.
+   * @param{string} message content to display
+   * @param{string} ariaLabel aria label of the toast icon
+   * @param{'error' | 'information' | 'success' | 'warning'} type type of toast notification
+   * @returns {EmberObject | void}
+   */
   addNotification({ message, ariaLabel, type }) {
-    if (!message || !ariaLabel) {
-      throw new Error('Mandatory attributes are missing: message and ariaLabel');
-    }
+    const isToastValid = Boolean(message && ariaLabel);
+
+    warn('Mandatory attributes are missing: message and ariaLabel', isToastValid, {
+      id: 'pix-ui.toast.not-message-or-ariaLabel',
+    });
+
+    if (!isToastValid) return;
 
     const toast = EmberObject.create({
       ariaLabel,
@@ -17,11 +28,17 @@ export default class ToastService extends Service {
       type: type || 'success',
     });
 
-    this.content.pushObject(toast);
+    this.content = [...this.content, toast];
 
     return toast;
   }
 
+  /**
+   * Creates and returns a new error toast notification.
+   * @param{string} message content to display
+   * @param{string} ariaLabel aria label of the toast icon
+   * @returns {EmberObject | void}
+   */
   sendErrorNotification({ message, ariaLabel }) {
     return this.addNotification({
       ariaLabel,
@@ -30,6 +47,12 @@ export default class ToastService extends Service {
     });
   }
 
+  /**
+   * Creates and returns a new success toast notification.
+   * @param{string} message content to display
+   * @param{string} ariaLabel aria label of the toast icon
+   * @returns {EmberObject | void}
+   */
   sendSuccessNotification({ message, ariaLabel }) {
     return this.addNotification({
       ariaLabel,
@@ -38,6 +61,12 @@ export default class ToastService extends Service {
     });
   }
 
+  /**
+   * Creates and returns a new information toast notification.
+   * @param{string} message content to display
+   * @param{string} ariaLabel aria label of the toast icon
+   * @returns {EmberObject | void}
+   */
   sendInformationNotification({ message, ariaLabel }) {
     return this.addNotification({
       ariaLabel,
@@ -46,6 +75,12 @@ export default class ToastService extends Service {
     });
   }
 
+  /**
+   * Creates and returns a new warning toast notification.
+   * @param{string} message content to display
+   * @param{string} ariaLabel aria label of the toast icon
+   * @returns {EmberObject | void}
+   */
   sendWarningNotification({ message, ariaLabel }) {
     return this.addNotification({
       ariaLabel,
@@ -54,11 +89,24 @@ export default class ToastService extends Service {
     });
   }
 
+  /**
+   * Remove toast notification from content list
+   * @param{{message: string, ariaLabel: string, type: string}} toast toast to remove
+   * @returns {EmberObject | void}
+   */
   removeNotification(toast) {
-    if (!toast) {
-      return;
-    }
+    if (!toast) return;
+    console.log(toast);
+    const toastIndex = this.content.findIndex(
+      (value) =>
+        toast.ariaLabel === value.ariaLabel &&
+        toast.message === value.message &&
+        toast.type === value.type,
+    );
 
-    this.content.removeObject(toast);
+    if (toastIndex < 0) return;
+
+    this.content.splice(toastIndex, 1);
+    this.content = [...this.content];
   }
 }
