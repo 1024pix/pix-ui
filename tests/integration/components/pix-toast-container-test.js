e18827b74bc5bead459614e3243e06d5cd41aa54
Eric Lim <eric.lim@zenika.com>
2024-11-06 11:58:29 +0100

    feat(mon-pix): add unit test for pix-toast-container component

---
+++ tests/integration/components/pix-toast-container-test.js
@@ -0,0 +1,39 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@1024pix/ember-testing-library';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | toast-container', function (hooks) {
+  setupRenderingTest(hooks);
+
+  module('when toast service content is not empty', function (hooks) {
+    let toasts = [];
+    hooks.beforeEach(function () {
+      toasts.push(
+        {
+          ariaLabel: 'close',
+          message: 'this is a toast',
+          type: 'success',
+        },
+        {
+          ariaLabel: 'close',
+          message: 'this is another toast',
+          type: 'error',
+        },
+      );
+
+      const pixToastService = this.owner.lookup('service:pixToast');
+      pixToastService.addNotification(toasts[0]);
+      pixToastService.addNotification(toasts[1]);
+    });
+
+    test('it renders its content as toast component', async function (assert) {
+      // when
+      const screen = await render(hbs`<PixToastContainer />`);
+      // then
+      assert.dom(screen.queryByText(toasts[0].message)).exists();
+      assert.dom(screen.queryByText(toasts[1].message)).exists();
+      assert.strictEqual(screen.getAllByRole('alert').length, 2);
+    });
+  });
+});
