00999b80b131389df62f0500764b102b7352b90a
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-12-02 15:38:46 +0100

    fix(pix-structure-witcher): remove unecessary wait for element

--- addon/components/pix-structure-switcher.js
+++ addon/components/pix-structure-switcher.js
@@ -2,18 +2,11 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
-import { inject as service } from '@ember/service';
-
 export default class PixStructureSwitcher extends Component {
-  @service elementHelper;
-
   constructor(...args) {
     super(...args);
     this.switcherId = 'structure-switcher-' + guidFor(this);
     this.listId = `listbox-${this.switcherId}`;
-    this.elementHelper.waitForElement(`container-${this.switcherId}`).then((element) => {
-      this.rootElement = element;
-    });
   }
 
   @tracked
