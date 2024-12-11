3115209b0e250039560077c5b04f4b1ad65de1ea
Eithliu <iris.benoit@pix.fr>
2024-12-11 12:00:33 +0100

    feat(table): add @onSort on pix table column
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Fael Bassetti <fael.bassetti@pix.fr>
    
    # Conflicts:
    #	addon/components/pix-table-column.js

--- addon/components/pix-table-column.js
+++ addon/components/pix-table-column.js
@@ -6,6 +6,69 @@ export default class PixTableColumn extends Component {
     return this.args.context === 'header';
   }
 
+  get sortable() {
+    return Boolean(this.args.onSort);
+  }
+
+  get sortOrder() {
+    if (this.args.sortOrder === undefined) {
+      return undefined;
+    }
+    const correctSortOrders = ['asc', 'desc', null];
+    warn(
+      'PixTableColumn: you need to provide a valid sortOrder',
+      correctSortOrders.includes(this.args.sortOrder),
+      {
+        id: 'pix-ui.table-column.sortOrder.not-valid',
+      },
+    );
+    return this.args.sortOrder;
+  }
+
+  get iconName() {
+    if (!this.sortOrder) {
+      return 'sort';
+    }
+    if (this.sortOrder === 'asc') {
+      return 'sortAsc';
+    }
+    return 'sortDesc';
+  }
+
+  get iconLabel() {
+    warn(
+      'PixTableColumn: parameters `@ariaLabelDefaultSort`, `@ariaLabelSortDesc` and `@ariaLabelSortAsc` are required for sort buttons',
+      ![
+        this.args.ariaLabelDefaultSort,
+        this.args.ariaLabelSortDesc,
+        this.args.ariaLabelSortAsc,
+      ].includes(undefined),
+      {
+        id: 'pix-ui.pix-table-column.sortAriaLabels.required',
+      },
+    );
+    if (!this.sortOrder) {
+      return this.args.ariaLabelDefaultSort;
+    }
+    if (this.sortOrder === 'asc') {
+      return this.args.ariaLabelSortDesc;
+    }
+    return this.args.ariaLabelSortAsc;
+  }
+
+  get ariaSort() {
+    if (!this.sortable) {
+      return undefined;
+    }
+    if (!this.sortOrder) {
+      return 'none';
+    }
+    if (this.sortOrder === 'asc') {
+      return 'ascending';
+    }
+    return 'descending';
+  }
+
   get typeClass() {
     const correctTypes = ['number', 'text'];
     const type = this.args.type ?? 'text';
