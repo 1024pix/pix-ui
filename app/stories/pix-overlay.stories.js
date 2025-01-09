146be50e93506f82e52e1907b94372ae1a5e1007
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2025-01-09 10:07:39 +0100

    feat(PixOverlay): add component story

---
+++ app/stories/pix-overlay.stories.js
@@ -0,0 +1,26 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+export default {
+  title: 'Other/Overlay',
+  argTypes: {
+    isVisible: {
+      name: 'isVisible',
+      description: "Visibilité de l'overlay",
+      type: { name: 'bool', required: false },
+      table: { defaultValue: { summary: true } },
+      control: { type: 'radio' },
+      options: [true, false],
+    },
+    onClose: {
+      name: 'onClose',
+      description: "Callback déclenché à la fermeture de l'overlay",
+    },
+  },
+};
+
+export const overlay = (args) => ({
+  template: hbs`<PixOverlay @isVisible={{this.isVisible}}>
+  Du contenu en dessous de l'overlay.
+</PixOverlay>`,
+  context: args,
+});
