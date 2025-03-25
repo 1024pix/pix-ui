5d619b9823ece34000c071f123582806a1d67674
alicegoarnisson <alice.goarnisson@pix.fr>
2025-03-25 16:19:27 +0100

    feat(pix-ui): gauge storybook

---
+++ app/stories/pix-gauge.stories.js
@@ -0,0 +1,53 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+export default {
+  title: 'Data display/Gauge',
+  argTypes: {
+    label: {
+      description: "Titre à renseigner pour les lecteurs d'écran",
+      type: { name: 'string', required: true },
+    },
+    stepLabels: {
+      description:
+        'Labels pour les différents niveaux (ex: Novice / Intermédiaire / Avancé / Expert)',
+      type: { name: 'array', required: false },
+    },
+    isSmall: {
+      description:
+        'A passer à true pour passer la jauge en taille "mini" + pour cacher les labels de niveaux et les séparateurs',
+      type: { name: 'boolean', required: false },
+    },
+    hiveValues: {
+      description: 'A passer à true pour cacher les valeurs numériques',
+      type: { name: 'boolean', required: false },
+    },
+    maxLevel: {
+      description: 'Niveau maximum atteignable',
+      type: { name: 'number', required: true },
+    },
+    reachedLevel: {
+      description: 'Niveau atteint',
+      type: { name: 'number', required: false },
+    },
+  },
+  args: {
+    stepLabels: ['Novice', 'Intermédiaire', 'Avancé', 'Expert'],
+    isSmall: false,
+    hideValues: false,
+    maxLevel: 4,
+    reachedLevel: 1,
+  },
+};
+
+export const PixGauge = (args) => {
+  return {
+    template: hbs`<PixGauge
+  @stepLabels={{this.stepLabels}}
+  @isSmall={{this.isSmall}}
+  @hideValues={{this.hideValues}}
+  @maxLevel={{this.maxLevel}}
+  @reachedLevel={{this.reachedLevel}}
+/>`,
+    context: args,
+  };
+};
