9fac218b31e066d9144ebb8e52639c52a114470e
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2025-04-25 14:15:06 +0200

    chore(pix-ui): config test environment

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,15 +2,15 @@ import { setApplication } from '@ember/test-helpers';
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 
 import { contains } from './helpers/contains';
 
-setApplication(Application.create(config.APP));
-
 setup(QUnit.assert);
-
+setApplication(Application.create(config.APP));
+loadTests();
 start();
 
 QUnit.assert.contains = contains;
