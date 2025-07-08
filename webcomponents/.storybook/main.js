87e1d64d0ed6a1ac3b3c9407f0aec4b006dfa482
Fael BASSETTI <fael.bassetti@pix.fr>
2025-07-08 17:01:13 +0200

    feat: add storybook to webcomponents

---
+++ webcomponents/.storybook/main.js
@@ -0,0 +1,20 @@
+
+
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  }
+};
+export default config;
--- webcomponents/vite.config.js
+++ webcomponents/vite.config.js
@@ -1,26 +1,54 @@
+/// <reference types="vitest/config" />
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          // traiter toutes les balises avec un tiret comme des éléments personnalisés
-          isCustomElement: (tag) => tag.includes('-'),
-        },
-      },
-    }),
-  ],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        // traiter toutes les balises avec un tiret comme des éléments personnalisés
+        isCustomElement: tag => tag.includes('-')
+      }
+    }
+  })],
   build: {
     lib: {
       entry: resolve(import.meta.dirname, './src/main.js'),
       name: 'PixUI',
       // the proper extensions will be added
-      fileName: 'pix-ui',
+      fileName: 'pix-ui'
     },
-    target: ['es2015'],
+    target: ['es2015']
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.js']
+      }
+    }]
+  }
 });
