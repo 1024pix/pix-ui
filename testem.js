c47ab697f94d3cff08ac8a31477e2dfe870245f7
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-09-04 23:28:57 +0200

    fix(ci): testem.js flakies
    See https://github.com/1024pix/pix/pull/12301

--- testem.js
+++ testem.js
@@ -1,23 +1,34 @@
-'use strict';
-
-module.exports = {
+const config = {
   test_page: 'tests/index.html?hidepassed',
+  reporter: 'dot',
   disable_watching: true,
   launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
-    Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
-    },
+    Chrome: [
+      // --no-sandbox is needed when running Chrome inside a container
+      process.env.CI ? '--no-sandbox' : null,
+      '--headless',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-software-rasterizer',
+      '--disable-accelerated-2d-canvas',
+      '--mute-audio',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900',
+    ].filter(Boolean),
   },
+  browser_disconnect_timeout: 20000,
+  timeout: 20000,
 };
+
+module.exports = process.env.CI
+  ? {
+      ...config,
+      reporter: 'xunit',
+      report_file: `${
+        process.env.RESULTS_PATH ?? './test-results/'
+      }/report.xml`,
+    }
+  : config;
